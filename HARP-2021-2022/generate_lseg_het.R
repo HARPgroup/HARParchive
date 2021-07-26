85de5420faa68ed94a710939ccf894e7bd016e6c
kylewlowe <kylel@vt.edu>
2021-07-26 11:32:31 -0400

    added updated hamon pet calculation script

---
+++ HARP-2021-2022/generate_lseg_het.R
@@ -0,0 +1,91 @@
+##### This script is an outline of calculating PET using the Hamon Method
+## Last Update 7/28/21
+## HARP Group
+
+library(lubridate)
+library(sqldf)
+
+# list of all land segments in VA's minor basins
+AllLandsegList <- c("N51800", "N51550", "N51810", "N51037", "N51093", "N51111", "N51053", "N51740", "N51710", "N51121", "N51135", "N51149", 
+                    "N51181", "N51650", "N51700", "N51161", "N51019", "N51031", "N51147", "N51199", "N51735", "N51131", "N51071", "N51770", 
+                    "N51011", "N51007", "N51041", "N51570", "N51730", "N51036", "N51095", "N51830", "N51073", "N51045", "N51775", "N51023", 
+                    "N51515", "N51680", "N51029", "N51049", "N51087", "N51670", "N51115", "H51023", "N51009", "N51145", "N51760", "N51127", 
+                    "N51097", "N51001", "H51045", "N51005", "L51023", "N51163", "N51125", "N51075", "N51085", "N51101", "N51119", "N51103", 
+                    "N51580", "L51163", "N51530", "H51009", "N51003", "N51065", "N51109", "N51057", "N51133", "N51017", "N51678", "H51125", 
+                    "N51033", "N51159", "N51015", "H51015", "N51177", "N51193", "L51015", "N51790", "N51820", "H51003", "N51540", "N51137", 
+                    "N24037", "N51091", "L51091", "H51165", "N51165", "L51079", "N51079", "N51113", "N51179", "N51099", "H51079", "N51047", 
+                    "N51630", "N24017", "N54071", "N51660", "N51139", "H51139", "H51113", "N51061", "N51153", "H54071", "L54071", "N51171", 
+                    "H51157", "N51157", "N51059", "N24033", "L51157", "N51683", "L54023", "N54023", "N54031", "H54031", "N51187", "N51107", 
+                    "N51685", "N51600", "N51013", "N51510", "N51610", "N11001", "N54093", "H54023", "L54031", "N51043", "N24031", "N54027", 
+                    "N51069", "N54077", "H24023", "N24023", "N54057", "N51840", "N54037", "N24021", "N24005", "N54003", "N24043", "N24013", 
+                    "N24001", "N54065", "H24021", "N42111", "N42009", "N42057", "N42055", "N42001", "N42041", "N42099", "N10001", "N10005", 
+                    "N24011", "N24019", "N24039", "N24045", "N24047", "A37001", "A37005", "A37009", "A37033", "A37067", "A37077", "A37081", 
+                    "A37131", "A37135", "A37145", "A37157", "A37169", "A37171", "A37181", "A37185", "A37189", "A47091", "A47163", "A51021", 
+                    "A51025", "A51027", "A51035", "A51051", "A51063", "A51067", "A51077", "A51081", "A51083", "A51089", "A51105", "A51117", 
+                    "A51141", "A51143", "A51155", "A51167", "A51169", "A51173", "A51175", "A51183", "A51185", "A51191", "B51197", "B51195", 
+                    "B51191", "B51185", "B51173", "B51169", "B51167", "B51141", "B51105", "B51077", "B51067", "B51035", "B37009", "B37005", 
+                    "A51750", "A51720", "A51690", "A51640", "A51620", "A51595", "A51590", "A51520", "A51197", "A51195")
+
+# loop iterates through AllLandsegList and outputs 2 csv files, one for each PET method
+i <- 225
+while(i<=length(AllLandsegList)){
+  landseg <- AllLandsegList[i]
+  # read in land segment radiation data
+  dfRAD <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".RAD"), header = FALSE, sep = ",")
+  colnames(dfRAD) = c("year","month","day","hour","RAD")
+  dfRAD$date <- as.Date(paste(dfRAD$year,dfRAD$month,dfRAD$day,sep="-"))
+  # create dataframe with number of daylight hours for each day
+  dfDLH <- sqldf("SELECT year, month, day, count(RAD) daylight_hrs
+                  FROM dfRAD
+                  WHERE rad > 0 
+                  GROUP BY date")
+  # read in land segment temperature data
+  dfTMP <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".TMP"), header = FALSE, sep = ",")
+  colnames(dfTMP) = c("year","month","day","hour","temp")
+  
+  
+  # hours with no radiation get daylight values of 0 while hours with radiation get values of 1
+  # these will be used later to make PET 0 if there is no radiation
+  hourlyPET <- data.frame(date = dfRAD$date,
+                          hour = dfRAD$hour,
+                          rad = dfRAD$RAD,
+                          daylight = "NA")
+  hourlyPET <- sqldf("SELECT date, hour, rad,
+                     CASE
+                     when rad == 0 then 0
+                     else 1
+                     end daylight
+                     FROM hourlyPET")
+  
+  # Hamon Equation
+  #N = daytime length
+  N <- (rep(dfDLH$daylight_hrs, 24))/12
+  # e = saturation vapor pressur
+  e = 6.108*exp((17.27*(dfTMP$temp)/((dfTMP$temp)+273.3)))
+  # k = 1 = proportioanlity coefficient (always equal to 1)
+  k <- 1
+  # t = average daily temperature 
+  t <- dfTMP$temp
+  # calculate PET
+  ET_H <- k*0.165*216.7*N*(e/(t+273.3))
+
+  # make PET values 0 if no solar radiation for given hour
+  hourlyPET$PET <- ET_H*as.numeric(hourlyPET$daylight)
+  # currently in mm/day, convert to in/hr
+  hourlyPET$PET <- hourlyPET$PET*0.0393701/24
+  
+  # add Hamon ET data to table with same convention as other metrics
+  dfHET <- data.frame(year = dfTMP$year, 
+                       month = dfTMP$month, 
+                       day = dfTMP$day,
+                       hour = dfTMP$hour,
+                       PET = hourlyPET$PET)
+  
+  # create and save HET file as csv
+  write.csv(dfHET,paste0("C:/Users/kylew/Documents/HARP/NLDAS/lseg_pet_csv/",landseg,".HET"), 
+              row.names = FALSE)
+  #write.csv(dfHET,paste0("/backup/meteorology/out/lseg_csv/1984010100-2020123123/",landseg,".HET"), 
+  #           row.names = FALSE)
+  
+  i<-i+1
+}
