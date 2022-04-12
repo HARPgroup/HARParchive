d18edf3ee2be18cd97a1c4209f8d57caa7fb5846
kylewlowe <kylel@vt.edu>
2022-04-12 18:55:25 +0100

    updating sumstats script

--- HARP-2021-2022/get_lseg_summary_stats.R
+++ HARP-2021-2022/get_lseg_summary_stats.R
@@ -25,18 +25,19 @@ while(i<=length(AllLandsegList)){
   # read in land segment temperature and precipitation data
   dfPRC <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".PRC"), header = FALSE, sep = ",")
   dfTMP <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".TMP"), header = FALSE, sep = ",")
-  dfHET <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".HET"), header = FALSE, sep = ",")
+  dfPET <- read.table(paste0("http://deq1.bse.vt.edu:81/met/out/lseg_csv/1984010100-2020123123/",landseg,".PET"), header = FALSE, sep = ",")
   #dfPRC <- read.table(paste0("/backup/meteorology/out/lseg_csv/1984010100-2020123123/",landseg,".PRC"), header = FALSE, sep = ",")
   #dfTMP <- read.table(paste0("/backup/meteorology/out/lseg_csv/1984010100-2020123123/",landseg,".TMP"), header = FALSE, sep = ",")
+  #dfPET <- read.table(paste0("/backup/meteorology/out/lseg_csv/1984010100-2020123123/",landseg,".PET"), header = FALSE, sep = ",")
   colnames(dfTMP) = c("year","month","day","hour","temp")
   dfTMP$date <- as.Date(paste(dfTMP$year,dfTMP$month,dfTMP$day, sep="-"))
   colnames(dfPRC) = c("year","month","day","hour","precip")
   dfPRC$date <- as.Date(paste(dfPRC$year,dfPRC$month,dfPRC$day, sep="-"))
-  colnames(dfHET) = c("year","month","day","hour","pet")
-  dfHET$date <- as.Date(paste(dfHET$year,dfHET$month,dfHET$day, sep="-"))
+  colnames(dfPET) = c("year","month","day","hour","pet")
+  dfPET$date <- as.Date(paste(dfPET$year,dfPET$month,dfPET$day, sep="-"))
   
   # run get_lseg_summary_stats function
-  summaryStats <- get_lseg_summary_stats(dfTMP=dfTMP,dfPRC=dfPRC,dfPET=dfHET)
+  summaryStats <- get_lseg_summary_stats(dfTMP=dfTMP,dfPRC=dfPRC,dfPET=dfPET)
   
   # create and save PET file as csv
   write.table(summaryStats,paste0("C:/Users/alexw/Documents/R/HARP/Summer 2021/landsegPETfiles/",landseg,"SummaryStats.csv"), 
