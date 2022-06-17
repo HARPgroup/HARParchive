a2b802c979157850e1f39e02cc4c1a73993f47a8
unknown <juliasb@vt.edu>
2022-06-17 08:49:44 -0400

    mapping exercise in R - edits by Julia

--- HARP-2022-Summer/RMappingExercise/RMappingPractice.R
+++ HARP-2022-Summer/RMappingExercise/RMappingPractice.R
@@ -1,6 +1,26 @@
+
 #R Mapping Excercise - Practice integrating mapping tools with R. 
 
+#Everything needed for a good map:
+  # - land (color 1)
+  # - watershed (color 2)
+  # - boundaries (color 3)
+  # - water (color 4 / can depend on depth or type)
+  # - points of interest (color 5, shape)
+        # * all colors should blend well with each other and distinguish the wanted qualities
+  # - appropriate frame
+  # - scale bar
+  # - north arrow
+  # - labels
+  # - legend (if needed)
+  # - elevation/hillshade (if needed)
+
 install.packages("classInt")
+install.packages("classint")
+install.packages("ggspatial")
+install.packages("sf")
+
+# reading in the libraries
 library(classInt) # needed this for sf package to work for me -GC
 library(sf)
 library(dplyr)
@@ -8,5 +28,25 @@ library(sf)
 library(ggplot2)
 library(ggspatial)
 
-river <- st_read("States/cb_2018_us_region_500k.shp") # adding in the river shapefile
+# reading in the data
+# samps are just extra points added to the map manually through an excel file that we don't have!
+river <- st_read("Watershed/NHDArea.shp") # adding in the river shapefile
 boundry <- st_read("Watershed/Shape/WBDHU6.shp") # adding the watershed boundary
+#samps <- read.csv("Site_Metadata_Cleaned.csv") # adding data points
+states <- st_read("States/cb_2018_us_region_500k.shp")
+virginia <- filter(states, NAME == "Virginia" | NAME == "Maryland") # only select state data on VA and MD
+fred <- data.frame(-77.4605, 38.3032, "Fredrickburg") # add in the location of Fredricksburg
+
+# creating the map
+ggplot() +
+  geom_sf(data=virginia, fill= "green", color= "black", lwd=0.75) +
+  geom_sf(data=boundry, fill=alpha("yellow", 0.2), color= "yellow", lwd=1.5) +
+  geom_sf(data=river, color= "darkblue", fill= "blue") +
+  geom_point(data=fred, x=fred[1], y=fred[2], pch=24, color="black", fill="red3", lwd=5) +
+  xlim(-78.5,-76) +
+  ylim(37.5,38.75) +
+  annotation_scale() +
+  annotation_north_arrow(height= ,) +
+  annotate(geom=text, x= -77.2, y= 38.85, label= "Fredricksburg", fontface= "bold", color= "white") +
+  theme(panel.background = element_rect(fill= 'lightblue'), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  
