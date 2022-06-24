8005983c9fcd39a104e560517e0ec6b8c2aa23ab
GCambridge <gavrielcambridge@gmail.com>
2022-06-24 14:51:29 -0400

    add python script to convert timestamps in h5 files

--- HARP-2022-Summer/RMappingExercise/RMappingPractice.R
+++ HARP-2022-Summer/RMappingExercise/RMappingPractice.R
@@ -30,10 +30,10 @@ library(ggspatial)
 
 # reading in the data
 # samps are just extra points added to the map manually through an excel file that we don't have!
-river <- st_read("Watershed/NHDArea.shp") # adding in the river shapefile
+river <- st_read("Watershed/Shape/NHDArea.shp") # adding in the river shapefile
 boundry <- st_read("Watershed/Shape/WBDHU6.shp") # adding the watershed boundary
 #samps <- read.csv("Site_Metadata_Cleaned.csv") # adding data points
-states <- st_read("States/cb_2018_us_region_500k.shp")
+states <- st_read("States/cb_2018_us_state_500k.shp")
 virginia <- filter(states, NAME == "Virginia" | NAME == "Maryland") # only select state data on VA and MD
 fred <- data.frame(-77.4605, 38.3032, "Fredrickburg") # add in the location of Fredricksburg
 
@@ -46,7 +46,7 @@ ggplot() +
   xlim(-78.5,-76) +
   ylim(37.5,38.75) +
   annotation_scale() +
-  annotation_north_arrow(height= ,) +
-  annotate(geom=text, x= -77.2, y= 38.85, label= "Fredricksburg", fontface= "bold", color= "white") +
+ # annotation_north_arrow(height= ,) 
+ # annotate(geom=text, x= -77.2, y= 38.85, label= 'Fredricksburg', fontface= "bold", color= "white") +
   theme(panel.background = element_rect(fill= 'lightblue'), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
   
