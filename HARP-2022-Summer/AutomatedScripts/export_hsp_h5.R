d4ff3861399c3c57704c16f2f283810d6d077b63
megpritch <104520247+megpritch@users.noreply.github.com>
2022-07-13 11:28:40 -0400

    removing .h5 after making .csv

--- HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R
+++ HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R
@@ -1,4 +1,5 @@
-#Script for exporting run data from an h5 to CSV
+
+# Script for exporting run data from an h5 to CSV
 # Script will have 3 inputs: h5_file_path, output_file_path, and data_source_table 
 
 library(rhdf5)
@@ -6,8 +7,8 @@ library(R.utils)
 
 # Accepting command arguments:
 argst <- commandArgs(trailingOnly = T)
-h5_file_path <- argst[1] # arguments are created as class 'character'
-output_file_path <- argst[2]
+h5_file_path <- argst[1] # arguments default as class 'character' ; name of the .h5
+output_file_path <- argst[2] #name of csv being created
 data_source_table <- argst[3] # Path to data table within h5 that begins with /[groupname]
 
 # Reading in table from h5
@@ -17,13 +18,8 @@ pwater <- H5Dread(did, bit64conversion = "double")
 origin <- "1970-01-01"
 pwater$index <- as.POSIXct((pwater$index)/10^9, origin = origin, tz = "UTC")
 
-#Exporting to a csv
+# Exporting to a csv
 write.table(pwater,file = output_file_path, sep = ",", row.names = FALSE) # Maybe .csv should be added to the end of file argument?
 
-
-
-
-
-
-
-
+# Deleting the .h5 used to make the .csv
+file.remove(h5_file_path)
