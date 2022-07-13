c2be670dae336050dbdf7ce5db49c9dc7e859f69
glenncampagna <gcampagna11@vt.edu>
2022-07-13 14:30:55 -0400

    revert, bug from output file path

--- HARP-2022-Summer/AutomatedScripts/hsp_pwater.R
+++ HARP-2022-Summer/AutomatedScripts/hsp_pwater.R
@@ -19,9 +19,8 @@ basepath='/var/www/R';
 source("/var/www/R/config.R") # will need file in same folder/directory
 # establishing location on server for storing images
 omsite = "http://deq1.bse.vt.edu:81"
-# output_file_path = 'out/land/p532sova_2021'
 # save_directory <-  "/var/www/html/data/proj3/out"
-save_directory <-  paste("/media/model/p532/",output_file_path, sep = '')
+save_directory <-  "/media/model/p532/out/land/p532sova_2021"
 #land_segment_name <- 'A51800' # need to remove before using on server 
 #scenario_name <- 'p532sova_2021'# need to remove before using on server 
 
@@ -30,8 +29,7 @@ argst <- commandArgs(trailingOnly = T)
 land_segment_name <- argst[1]
 scenario_name <- argst[2]
 pwater_file_path <- argst[3]
-output_file_path <-  argst[4] # 'out/land/p532sova_2021'
-landuse <- argst[5]
+landuse <- argst[4]
 
 pwater <- fread(pwater_file_path)
 pwater$date <- as.Date(pwater$index, format = "%m/%d/%y")
@@ -176,7 +174,7 @@ model_constant_agwo_Runit$save(TRUE)
 
 
 # Add code here to export graphs 
-save_url = paste(omsite,'/p532/', output_file_path, sep ='')
+save_url = paste(omsite,'/p532/out/land/p532sova_2021', sep ='')
 # For graph 1
 fname <- paste(
   save_directory,paste0(landuse,'',land_segment_name,'.', 'fig.AGWS', '.png'), # building file name
