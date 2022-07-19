30427e22543e0af9182af7387cae539a889f8d89
glenncampagna <gcampagna11@vt.edu>
2022-07-19 15:02:20 -0400

    update to same version as on server

--- HARP-2022-Summer/img_gen.bat
+++ HARP-2022-Summer/img_gen.bat
@@ -8,9 +8,9 @@ data_source_table='RESULTS/PERLND_P001/PWATER/table'. #This is the same for all 
 
 # check the directories
 if [ ! -d "$CBP_EXPORT_DIR" ] ; then mkdir $CBP_EXPORT_DIR; fi
-if [ ! -d "$CBP_EXPORT_DIR/$scenario_name" ] ; then mkdir $CBP_EXPORT_DIR/$scenario_name; fi
+if [ ! -d "$CBP_EXPORT_DIR/land/$scenario_name" ] ; then mkdir $CBP_EXPORT_DIR/land/$scenario_name; fi
 if [ ! -d "$CBP_EXPORT_DIR/land/$scenario_name"/eos ] ; then mkdir $CBP_EXPORT_DIR/land/$scenario_name/eos; fi
-if [ ! -d "$CBP_EXPORT_DIR/land/$scenario_name"/images ] ; then mkdir $CBP_EXPORT_DIR/land/$scenario_name/images; fi
+if [ ! -d "$CBP_EXPORT_DIR/land/$scenario_name"/images ] ; then mkdir $CBP_EXPORT_DIR/land/$scenario_name/images; >
 
 
 land_use_list=$(ls $CBP_ROOT/output/hspf/land/out)
@@ -24,19 +24,21 @@ echo $segments
       h5_file_path="$CBP_ROOT/output/hspf/land/out/$landuse/$scenario_name/$landuse$landseg.h5"
       #echo $h5_file_path
 
-      output_file_path="CBP_EXPORT_DIR/out/land/$scenario_name/eos/$landuse$landseg"_pwater.csv
+      output_file_path="$CBP_EXPORT_DIR/land/$scenario_name/eos/$landuse$landseg"_pwater.csv
       #echo $output_file_path
 
-      image_file_path="$CBP_EXPORT_DIR/out/land/$scenario_name/images"
+      image_file_path="$CBP_EXPORT_DIR/land/$scenario_name/images"
       #echo $image_file_path
 
-      #echo "Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_file_path $data_source_table"
-      Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_file_path $data_source_table
+      #echo "Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_file_pa>
+      Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_file_path $dat>
 
       #remove old .h5 file
       #del h5_file_path
-      #echo "Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/hsp_pwater.R $landseg $scenario_name $landuse $output_file_path $image_file_path"
-      Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/hsp_pwater.R $landseg $scenario $landuse $output_file_path $image_file_path
+      #echo "Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/hsp_pwater.R $landseg $scenario_name $landuse>
+      Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/hsp_pwater.R $landseg $scenario $landuse $image_file>
 
       done
   done
+
+
