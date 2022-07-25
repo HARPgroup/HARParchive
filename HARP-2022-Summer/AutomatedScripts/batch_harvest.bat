29726f6850c54874bed8a1000a60217d2c300b08
juliabruneau <104520318+juliabruneau@users.noreply.github.com>
2022-07-25 10:08:27 -0400

    Update batch_harvest.bat

--- HARP-2022-Summer/AutomatedScripts/batch_harvest.bat
+++ HARP-2022-Summer/AutomatedScripts/batch_harvest.bat
@@ -41,14 +41,16 @@ landvar_imp=($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/detect_da
 landvar_riv=($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/detect_data_source.R $h5_file_path $data_source_riv))
 
 
-if  [ $landvar_per -eq 1 ]; then
+if  [[ $landvar_per -eq 1 ]]; then
   Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_pwater.csv' $data_source_per ; fi
 echo 'pwater csv created'
-elif [ $landvar_imp -eq 1 ]; then
+fi
+if  [[ $landvar_imp -eq 1 ]]; then
   Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_iwater.csv' $data_source_imp ; fi
 echo 'iwater csv created'
-elif [ $landvar_riv -eq 1 ]; then
-  Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_hydr.csv' $data_source_riv ;
+fi
+if  [[ $landvar_riv -eq 1 ]]; then
+ Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_hydr.csv' $data_source_riv ;
 echo 'hydr csv created'
 fi
 
