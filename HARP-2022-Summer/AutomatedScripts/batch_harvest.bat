4a3173498a6f2bffa1fb6386d65a640855f9c821
glenncampagna <gcampagna11@vt.edu>
2022-07-25 07:06:28 -0400

    update harvest batch script to incorporate detect_data_source.R

--- HARP-2022-Summer/AutomatedScripts/batch_harvest.bat
+++ HARP-2022-Summer/AutomatedScripts/batch_harvest.bat
@@ -15,8 +15,9 @@ basin=$2
 echo 'CBP_ROOT:' $CBP_ROOT
 echo 'CBP_EXPORT_DIR:' $CBP_EXPORT_DIR
 
-data_source_per=/RESULTS/PERLND_P001/PWATER/table
-data_source_imp=/RESULTS/IMPLND_I001/IWATER/table
+data_source_per=/RESULTS/PERLND_P001/PWATER
+data_source_imp=/RESULTS/IMPLND_I001/IWATER
+data_source_riv=/RESULTS/RCHRES_R001/HYDR
 
 image_file_path=$CBP_EXPORT_DIR/land/$scenario_name/images
 echo 'image file path:' $image_file_path
@@ -30,24 +31,33 @@ segments=`cbp get_landsegs $basin`
   for landseg in $segments; do
     for landuse in $land_use_list; do
       h5_file_path=$CBP_ROOT/output/hspf/land/out/$landuse/$scenario_name/$landuse$landseg'.h5'
-      echo $h5_file_path
 
       output_path_list=$CBP_EXPORT_DIR/land/$scenario_name/pwater/$landuse$landseg
 
+landvar_per=($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/detect_data_source.R $h5_file_path $data_source_per))
 
-landvar=($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/detect_per_imp.R $h5_file_path))
-echo 'landvar=' $landvar
+landvar_imp=($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/detect_data_source.R $h5_file_path $data_source_imp))
 
-if [[ $landvar -eq 1 ]]  ; then
-echo 'Pervious'
-  Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_pwater.csv' $data_source_per
+landvar_riv=($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/detect_data_source.R $h5_file_path $data_source_riv))
 
-elif  [[ $landvar -eq 0 ]]; then
-echo 'Impervious'
-  Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_iwater.csv' $data_source_imp
+
+if  [[ $landvar_per -eq 1 ]]; then
+  Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_pwater.csv' $data_source_per ; fi
+echo 'pwater csv created'
 fi
+if  [[ $landvar_imp -eq 1 ]]; then
+  Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_iwater.csv' $data_source_imp ; fi
+echo 'iwater csv created'
+fi
+if  [[ $landvar_riv -eq 1 ]]; then
+ Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path_list'_hydr.csv' $data_source_riv ;
+
+echo 'hydr csv created'
+fi
+
 done
 done
+
 
 
 
