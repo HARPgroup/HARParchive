4e237f45fd13569bb36b56cd467cfac6fcefdd67
megpritch <104520247+megpritch@users.noreply.github.com>
2022-08-02 17:14:53 -0400

    ftable troubleshooting

--- HARP-2022-Summer/AutomatedScripts/ftable_creation.R
+++ HARP-2022-Summer/AutomatedScripts/ftable_creation.R
@@ -1,29 +1,25 @@
 
 # script that generates FTABLEs from existing river segments in VAHydro
 
-#setup
+# Setup
+library("hydrotools") #needed to pull values from VAHydro 
+
+# Link data source (ds) to VAHydro
 basepath='/var/www/R';
 source("/var/www/R/config.R") #will need file in same folder/directory
-library("hydrotools")
-library('zoo')
-library('lubridate')
+ds <- RomDataSource$new(site, rest_uname = rest_uname)
+ds$get_token(rest_pw)
 
 # Accepting command arguments:
 argst <- commandArgs(trailingOnly = T)
-hydroid <- argst[1]
-riverseg <- argst[2] 
-channel <- argst[3]
+riverseg <- argst[1] 
+channel <- argst[2]
 
 #Testing: comment these out later
-hydroid <- "68298"
 riverseg <- "OR1_7700_7980"
 channel<- "0. River Channel"
 
-# Set up our data source
-ds <- RomDataSource$new(site, rest_uname = rest_uname)
-ds$get_token(rest_pw)
 #----------------------------------------------------------------------------
-
 # Pulling from VAHydro
 rseg<- RomFeature$new(
   ds,
@@ -68,8 +64,8 @@ drainage_area <- RomProperty$new(
   ),
   TRUE
 )
-
 da <- drainage_area$propvalue #106.052
+
 
 province <- RomProperty$new(
   channel_prop[["datasource"]],
@@ -81,8 +77,8 @@ province <- RomProperty$new(
   ),
   TRUE
 )
+prov <- -1 #province$propvalue
 
-province <- province$propvalue
 
 length <- RomProperty$new(
   channel_prop[["datasource"]],
@@ -94,8 +90,8 @@ length <- RomProperty$new(
   ),
   TRUE
 )
+clength <- length$propvalue #channel length
 
-clength <- length$propvalue
 
 slope <- RomProperty$new(
   channel_prop[["datasource"]],
@@ -107,12 +103,11 @@ slope <- RomProperty$new(
   ),
   TRUE
 )
-cslope <- slope$propvalue #0.0016
+cslope <- slope$propvalue #longitudinal channel slope
 
 #----------------------------------------------------------------------------
-
 # Calculating Channel Geometry
-if (province == 1){
+if (prov == 1){
   #Appalachian Plateau
   hc = 2.030 # "c" = coefficient for regional regression eqn
   he = 0.2310 # "e" = exponent for regional regression eqn
@@ -123,7 +118,7 @@ if (province == 1){
   n = 0.036 # Manning's n
 }
 
-if (province ==2){
+if (prov == 2){
   #Valley and Ridge
   hc = 1.435
   he = 0.2830
@@ -134,7 +129,7 @@ if (province ==2){
   n = 0.038
 }
 
-if (province ==3){
+if (prov ==3){
   #Piedmont
   hc = 2.137
   he = 0.2561
@@ -145,7 +140,7 @@ if (province ==3){
   n = 0.095
 }
 
-if (province ==4){
+if (prov ==4){
   #Coastal Plain
   hc = 2.820
   he = 0.2000
@@ -156,7 +151,7 @@ if (province ==4){
   n = 0.040
 }
 
-if (province == -1){
+if (prov == -1){
   #Single/General Equation
   hc = 2.177
   he = 0.2293
@@ -164,7 +159,7 @@ if (province == -1){
   bfe = 0.4432
   bc = 5.471
   be = 0.5103
-  n = 0.05225 #avg of above n values
+  n = 0.05225 #avg of ^above n values
 }
   
 # Regional Regression Eqn's:
@@ -174,39 +169,42 @@ h = hc * (da**he)
 bf = bfc * (da**bfe)
 #base width "width @ lowest stage":
 b = bc * (da**be)
-#side slope:
+#side slope of channel:
 z = 0.5 * (bf - b ) / h
 
 #----------------------------------------------------------------------------
-
 # Calculating FTABLE
 
 # Depth
 #depth <- seq(0,h,length=19) #sequence between zero and bank full stage
-depth<- c(0.586,1.172,1.758, 2.343, 2.929, 3.515)
+depth<- c(.000, 0.586, 1.172, 1.758, 2.343, 2.929, 3.515, 4.687, 5.858, 7.030,
+          9.373, 11.717, 14.060, 16.403, 18.747, 21.090, 23.433, 25.777, 28.120)
 
 # Surface Area
     # water surface width * length of channel
-sw <- b+2*z*depth
-area <- (sw *clength)/43560 #converting from ft^2 to acres
+sw <- b + 2*z*depth
+sw[depth == 0] <- 0 # zero depth = zero surface water width
+area = (sw * clength)/43560 #converting to acres
 
 # Volume
     # length * cross sectional area
 vol <- (clength * (0.5*(sw+b)*depth))/43560 #converting to ft-acre
 
 # Discharge
-    # Q = V * A , A = cross sectional area / flow area
-    # Manning's: V = (1.49/n) * R^(2/3) * S^(1/2) 
-    # ^^ (1.49/n) is English ; (1/n) is metric
-    # Hydraulic radius = (depth*(b+z*depth))/(b+2*depth*sqrt(1+z^2))
-    # or Hydraulic R = (depth*(b + sw)/2)/(b + 2*(((sw-b)/2)**2 + depth**2)**0.5)
-disch <- (1.49/n) * ((depth*(b+z*depth))/(b+2*depth*sqrt(1+z^2)))**(2/3) * cslope**0.5 * 0.5*(sw+b)*depth
+    # Q = V * A ; where A = cross sectional area / flow area
+    # Manning's Eqn: V = (1.49/n) * R^(2/3) * S^(1/2) 
+        # (1.49/n) is English ; (1/n) is metric
+    # Hydraulic Radius = (depth*(b+z*depth))/(b+2*depth*sqrt(1+z^2))
+        # OR Hydraulic R = (depth*(b + sw)/2)/(b + 2*(((sw-b)/2)**2 + depth**2)**0.5)
+
+disch <- (1.49/n) * ((depth*(b+z*depth))/(b+2*depth*sqrt(1+z^2)))**(2/3) * 
+  cslope**0.5 * 0.5*(sw+b)*depth
 
 # Compile 
 ftable <- data.frame(depth, area, vol, disch)
-#colnames(ftable) <- ("Depth (ft)", "Area (acres)", "Volume (ac-ft)", "Discharge (cfs)")
-#----------------------------------------------------------------------------
+colnames(ftable) <- c('Depth (ft)', 'Area (acres)', 'Volume (ac-ft)', 'Discharge (cfs)')
 
+#----------------------------------------------------------------------------
 # Exporting to VAHydro
 
 #exp_ftable<- RomProperty$new(
@@ -221,3 +219,66 @@ ftable <- data.frame(depth, area, vol, disch)
 #exp_ftable$propvalue <- ftable
 #exp_ftable$save(TRUE)
 
+#----------------------------------------------------------------------------
+# Trying Some Plotting
+
+# Run w/ Province = 3:
+ftable_specific <- data.frame(depth, area, vol, disch)
+# Run w/ Province = -1:
+ftable_generic <- data.frame(depth, area, vol, disch)
+
+# Copying UCI table
+uci_area <- c(.000, 163.720, 173.714, 183.708, 193.702, 203.696, 213.690, 233.677, 253.665,273.653,
+              639.118, 1004.584, 1370.049, 1735.514, 2100.980, 2466.445, 2831.911,3197.376, 3562.842)
+uci_vol <- c(.0000, 92.99, 191.83, 296.52, 407.07, 523.47, 645.73, 907.82, 1193.32, 1502.24, 2571.70,
+             4497.57, 7279.85, 10918.54, 15413.63, 20765.13, 26973.04, 34037.36, 41958.08)
+uci_disch <- c(.000, 12.10, 41.80, 96.90, 165.00, 245.00, 338.00, 554.00, 794.00, 1080.00, 1960.00,
+               3460.00, 5510.00, 7950.00, 10400.00, 14200.00, 22507.24, 27110.33, 32149.80)
+ftable_uci <- data.frame(depth, uci_area, uci_vol, uci_disch)
+colnames(ftable_uci) <- c('depth', 'area', 'vol', 'disch')
+
+# Zoomed Out:
+  # Area
+par(mfrow = c(1,3))
+plot(ftable_uci$depth, ftable_uci$area, type='l', col = 'red', ylim=c(0, 3562.842), xlab = "Depth (ft)", ylab = "Water Surface Area (acres)")
+lines(ftable_specific$depth, ftable_specific$area, type='l', col='dark green')
+lines(ftable_generic$depth, ftable_generic$area, type='l', col='blue')
+title(main = 'UCI vs Calculated Ftables: Area')
+legend(x=0 , y=3000 , legend= c("uci", "specific equations", "generic equations"), col=c('red', 'dark green', 'blue'),bty='n',lty=1, cex=1.25)
+
+  # Volume
+plot(ftable_uci$depth, ftable_uci$vol, type='l', col = 'red', ylim=c(0, 41959), xlab = "Depth (ft)", ylab = "Volume (acre-ft)")
+lines(ftable_specific$depth, ftable_specific$vol, type='l', col='dark green')
+lines(ftable_generic$depth, ftable_generic$vol, type='l', col='blue')
+title(main = 'UCI vs Calculated Ftables: Volume')
+legend(x=0 , y=35000 , legend= c("uci", "specific equations", "generic equations"), col=c('red', 'dark green', 'blue'),bty='n',lty=1, cex = 1.25)
+
+  # Discharge
+plot(ftable_uci$depth, ftable_uci$disch, type='l', col = 'red', ylim=c(0, 32745), xlab = "Depth (ft)", ylab = "Discharge (cfs)")
+lines(ftable_specific$depth, ftable_specific$disch, type='l', col='dark green')
+lines(ftable_generic$depth, ftable_generic$disch, type='l', col='blue')
+title(main = 'UCI vs Calculated Ftables: Discharge')
+legend(x=0 , y=30000 , legend= c("uci", "specific equations", "generic equations"), col=c('red', 'dark green', 'blue'),bty='n',lty=1, cex=1.25)
+
+# Zooming In:
+  # Area
+par(mfrow = c(1,3))
+plot(ftable_uci$depth, ftable_uci$area, type='l', col = 'red', xlim=c(0,8), ylim=c(0,350), xlab = "Depth (ft)", ylab = "Water Surface Area (acres)")
+lines(ftable_specific$depth, ftable_specific$area, type='l', col='dark green')
+lines(ftable_generic$depth, ftable_generic$area, type='l', col='blue')
+title(main = 'UCI vs Calculated Ftables: Area')
+legend(x=2 , y=100 , legend= c("uci", "specific equations", "generic equations"), col=c('red', 'dark green', 'blue'),bty='n',lty=1, cex=1.25)
+
+  # Volume
+plot(ftable_uci$depth, ftable_uci$vol, type='l', col = 'red', xlim=c(0,8), ylim=c(0, 2300), xlab = "Depth (ft)", ylab = "Volume (acre-ft)")
+lines(ftable_specific$depth, ftable_specific$vol, type='l', col='dark green')
+lines(ftable_generic$depth, ftable_generic$vol, type='l', col='blue')
+title(main = 'UCI vs Calculated Ftables: Volume')
+legend(x=0 , y=2000 , legend= c("uci", "specific equations", "generic equations"), col=c('red', 'dark green', 'blue'),bty='n',lty=1, cex = 1.25)
+
+  # Discharge
+plot(ftable_uci$depth, ftable_uci$disch, type='l', col = 'red', xlim=c(0,8), ylim=c(0, 3000), xlab = "Depth (ft)", ylab = "Discharge (cfs)")
+lines(ftable_specific$depth, ftable_specific$disch, type='l', col='dark green')
+lines(ftable_generic$depth, ftable_generic$disch, type='l', col='blue')
+title(main = 'UCI vs Calculated Ftables: Discharge')
+legend(x=0 , y=2500 , legend= c("uci", "specific equations", "generic equations"), col=c('red', 'dark green', 'blue'),bty='n',lty=1, cex=1.25)
