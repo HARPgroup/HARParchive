984b26423fa17936958599bcdb8a02d9fdf740ac
glenncampagna <gcampagna11@vt.edu>
2022-08-08 14:49:12 -0400

    adjustments to perform hourly analysis

--- HARP-2022-Summer/compare_hspf_hsp2.R
+++ HARP-2022-Summer/compare_hspf_hsp2.R
@@ -18,7 +18,7 @@ suppressPackageStartupMessages(library(R.utils))
 omsite = "http://deq1.bse.vt.edu:81"
 
 #setwd("/Users/glenncampagna/Desktop/HARPteam22/Data") # for testing only 
-#hydr_2 <- fread("OR1_7700_7980_hydr.csv") # for testing only 
+#hydr_2 <- fread("OR1_7700_7980_hydr.csv") # for testing only #
 #hydr_f <- fread("OR1_7700_7980_0111.csv") # for testing only 
 
 # Accepting command arguments:
@@ -26,10 +26,10 @@ argst <- commandArgs(trailingOnly = T)
 path_to_hspf <- argst[1]
 path_to_hsp2 <- argst[2] 
 
-image_path1 <- '/media/model/p532/out/river/hsp2_2022/images/OR1_7700_7980_compare.png'
+#image_path1 <- '/media/model/p532/out/river/hsp2_2022/images/OR1_7700_7980_compare.png'
 image_path2 <- '/media/model/p532/out/river/hsp2_2022/images/OR1_7700_7980_compare.pd.box.png'
 image_path3 <- '/media/model/p532/out/river/hsp2_2022/images/OR1_7700_7980_compare.pd.flow.png'
-image_path4 <- '/media/model/p532/out/river/hsp2_2022/images/OR1_7700_7980_compare.pd.mon.png'
+#image_path4 <- '/media/model/p532/out/river/hsp2_2022/images/OR1_7700_7980_compare.pd.mon.png'
 
 
 #Importing flow data from hspf:
@@ -41,74 +41,83 @@ hydr_2 <- fread(path_to_hsp2) # Hydr table from hsp2 - ROVOL in ac-ft/hr
 
 colnames(hydr_f) <- c('year', 'month', 'day', 'hour', 'flow')
 
+
 #Convert ROVOL to cfs for comparison (from ac-ft/hr)
 hydr_2$Qout_cfs = hydr_2$ROVOL*12.1
 hydr_f$Qout_cfs = hydr_f$flow*12.1
 
+hydr_f_ <- hydr_f[2:nrow(hydr_f)] #Results in hours lining up for hydr_f_ and hydr_2
+hydr_f_$Qout2 <- hydr_2$Qout_cfs[2:(nrow(hydr_f_)+1)]
+hydr_f_$diff <- (abs(hydr_f_$Qout_cfs - hydr_f_$Qout2))/((hydr_f_$Qout_cfs + hydr_f_$Qout2)/2)*100
+
+
+#Removing the 1st 9 months of data because of hsp2 'warm up period'
+hydr_f_warm <- hydr_f_[6577:nrow(hydr_f_)]
+avg_diff <- mean(hydr_f_warm$diff)
+
+
+
 #Adding usable dates to both data tables 
-hydr_2$date <- as.Date(hydr_2$index, format = "%m/%d/%Y %H:%M")
-hydr_2$month <- month(hydr_2$date)
-hydr_2$year <- year(hydr_2$date)
-hydr_f$date = paste(hydr_f$year,'-',hydr_f$month,'-',hydr_f$day, sep = '')
+#hydr_2$date <- as.Date(hydr_2$index, format = "%m/%d/%Y %H:%M")
+#hydr_2$month <- month(hydr_2$date)
+#hydr_2$year <- year(hydr_2$date)
+#hydr_f$date = paste(hydr_f$year,'-',hydr_f$month,'-',hydr_f$day, sep = '')
 
 #Aggregate both tables to get monthly average flows 
-monthlyQout2 <- aggregate(hydr_2$Qout_cfs, by = list(hydr_2$month, hydr_2$year), FUN = "mean")
-colnames(monthlyQout2) <- c("month", "year", "Qout")
+#monthlyQout2 <- aggregate(hydr_2$Qout_cfs, by = list(hydr_2$month, hydr_2$year), FUN = "mean")
+#colnames(monthlyQout2) <- c("month", "year", "Qout")
 
-monthlyQoutF <- aggregate(hydr_f$Qout_cfs, by = list(hydr_f$month, hydr_f$year), FUN = "mean")
-colnames(monthlyQoutF) <- c("month", "year", "Qout")
+#monthlyQoutF <- aggregate(hydr_f$Qout_cfs, by = list(hydr_f$month, hydr_f$year), FUN = "mean")
+#colnames(monthlyQoutF) <- c("month", "year", "Qout")
 
-monthlyQout2$date <- paste(monthlyQout2$month,'-',monthlyQout2$year, sep = '')
-monthlyQoutF$date <- paste(monthlyQoutF$month,'-',monthlyQoutF$year, sep = '')
+#monthlyQout2$date <- paste(monthlyQout2$month,'-',monthlyQout2$year, sep = '')
+#monthlyQoutF$date <- paste(monthlyQoutF$month,'-',monthlyQoutF$year, sep = '')
 
-monthlyQout2$num <- 1:nrow(monthlyQout2)
-monthlyQoutF$num <- 1:nrow(monthlyQoutF)
+#monthlyQout2$num <- 1:nrow(monthlyQout2)
+#monthlyQoutF$num <- 1:nrow(monthlyQoutF)
 
-max2 <- max(monthlyQout2$Qout)
-maxf <- max(monthlyQoutF$Qout)
-if (max2 > maxf) {
-  max = max2
-}
-if (maxf > max2) {
+#max2 <- max(monthlyQout2$Qout)
+#maxf <- max(monthlyQoutF$Qout)
+#if (max2 > maxf) {
+#  max = max2
+#}
+#if (maxf > max2) {
   max = maxf
 }
-monthlyQout_clip <- monthlyQout2[1:nrow(monthlyQoutF), ]
-monthlyQout_clip$QoutF <- monthlyQoutF$Qout
-years <- seq(min(monthlyQout_clip$year), max(monthlyQout_clip$year)) # for graphing
+#monthlyQout_clip <- monthlyQout2[1:nrow(monthlyQoutF), ]
+#monthlyQout_clip$QoutF <- monthlyQoutF$Qout
+#years <- seq(min(monthlyQout_clip$year), max(monthlyQout_clip$year)) # for graphing
 
-png(image_path1)
-plot(monthlyQout_clip$num,monthlyQout_clip$Qout, type = 'l', col = 'blue', ylab = 'Qout (cfs)',  xlab = NA, xaxt = 'n', ylim = c(0,max))
-lines(monthlyQoutF$num, monthlyQoutF$Qout, col = 'red')
-legend(x = "topright", legend = c('HSP2', 'HSPF'), fill = c('blue','red'), bty = 'n')
-title(main = 'HSPF vs HSP2 Outflows', sub = 'Monthly average values are plotted')
-axis(1, at = seq(4,nrow(monthlyQout_clip),12), labels = years)
+#png(image_path1)
+#plot(monthlyQout_clip$num,monthlyQout_clip$Qout, type = 'l', col = 'blue', ylab = 'Qout (cfs)',  xlab = NA, xaxt = 'n', ylim = c(0,max))
+#lines(monthlyQoutF$num, monthlyQoutF$Qout, col = 'red')
+#legend(x = "topright", legend = c('HSP2', 'HSPF'), fill = c('blue','red'), bty = 'n')
+#title(main = 'HSPF vs HSP2 Outflows', sub = 'Monthly average values are plotted')
+#axis(1, at = seq(4,nrow(monthlyQout_clip),12), labels = years)
 
 
 #Percent differences:
-monthlyQout_clip$pct_df <- (abs(monthlyQout_clip$Qout - monthlyQout_clip$QoutF))/((monthlyQout_clip$QoutF+monthlyQout_clip$Qout)/2)*100
-pc_avg = as.numeric(mean(monthlyQout_clip$pct_df))
-pc_rd <- round(pc_avg, digits = 2)
 
-print(paste('Avg % difference:', pc_rd))
+print(paste('Avg % difference:', round(avg_diff, digits = 7)))
 
 png(image_path2)
-boxplot(monthlyQout_clip$pct_df, ylab = '% Difference')
+boxplot(hydr_f_warm$diff, ylab = '% Difference')  # Not useful for hourly data
 title(main='Percent difference of Outflow (HSPF - HSP2)')
 
-monthlyQout_clip$sum <- monthlyQout_clip$Qout + monthlyQout_clip$QoutF
+#monthlyQout_clip$sum <- monthlyQout_clip$Qout + monthlyQout_clip$QoutF
 
 png(image_path3)
-plot(monthlyQout_clip$sum,monthlyQout_clip$pct_df, xlab = 'Magnitude of Flow (cfs)', ylab = '% Difference', pch =18) +
+plot((hydr_f_warm$Qout_cfs+hydr_f_warm$Qout2),hydr_f_warm$diff, xlab = 'Magnitude of Flow (cfs)', ylab = '% Difference') +
 title(main='% difference as a function of Outflow (HSPF - HSP2)')
 
-pc_mon <- aggregate(monthlyQout_clip$pct_df, by = list(monthlyQout_clip$month), FUN = 'mean')
-colnames(pc_mon) <- c('month', 'pc')
-pc_mon$mon <- month.abb
+#pc_mon <- aggregate(monthlyQout_clip$pct_df, by = list(monthlyQout_clip$month), FUN = 'mean')
+#colnames(pc_mon) <- c('month', 'pc')
+#pc_mon$mon <- month.abb
 
-png(image_path4)
-ggplot(pc_mon, aes(x=reorder(month,mon),pc)) + geom_bar(stat='identity') + 
-  ggtitle('Avg % difference in outflow by month (HSPF - HSP2)') +
-  labs(x=NULL,y='% Difference') + 
-  annotate(geom = "label", x=3, y=0.5, label = paste("Overall avg % diff:", pc_rd)) +
-  scale_x_discrete( labels = month.abb) 
+#png(image_path4)
+#ggplot(pc_mon, aes(x=reorder(month,mon),pc)) + geom_bar(stat='identity') + 
+#  ggtitle('Avg % difference in outflow by month (HSPF - HSP2)') +
+#  labs(x=NULL,y='% Difference') + 
+#  annotate(geom = "label", x=3, y=0.5, label = paste("Overall avg % diff:", pc_rd)) +
+#  scale_x_discrete( labels = month.abb) 
 
