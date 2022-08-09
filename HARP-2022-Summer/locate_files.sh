4ee6e1017bc545b7ede5d3b1b77bd209357f9019
glenncampagna <gcampagna11@vt.edu>
2022-08-09 14:13:28 -0400

    use args now instead of read command

--- HARP-2022-Summer/locate_files.sh
+++ HARP-2022-Summer/locate_files.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 #this script should be run from directory: /opt/model/p53/p532c-sova
-echo -n 'River or Land segment? (river/land) '
-read segvar
+
+segvar=$1
 
 if [ "$segvar" == "land" ]; then
-echo -n 'scenario: '
-read scenario_name
-echo -n 'landseg: '
-read landseg
-echo -n 'landuse: '
-read landuse
+scenario_name=$2
+landseg=$3
+landuse=$4
 fi
 
 if [ "$segvar" == "river" ]; then
-echo -n 'scenario: '
-read scenario_name
-echo -n 'riverseg: '
-read riverseg
+scenario_name=$2
+riverseg=$3
 fi
 
 . hspf_config #Loads version-specific cbp variables
@@ -44,6 +39,7 @@ if  [ ! -f $h5_land ] && [ ! -f $h5_land_root ]; then
 echo 'Land seg h5 does not exist at' $CBP_EXPORT_DIR/land/$scenario_name/h5 'or' $CBP_ROOT/output/hspf/land/out/$landuse/$scenario_name ;
 fi
 fi
+
 
 
 
