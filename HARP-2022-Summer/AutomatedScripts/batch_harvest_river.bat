c89c85ad6360c99a44161862451d786dc5b96ca6
unknown <juliasb@vt.edu>
2022-08-12 14:32:59 -0400

    edited batch file

--- HARP-2022-Summer/AutomatedScripts/batch_harvest_river.bat
+++ HARP-2022-Summer/AutomatedScripts/batch_harvest_river.bat
@@ -11,8 +11,9 @@ basin=$2
  if [ ! -d $CBP_EXPORT_DIR ] ; then mkdir $CBP_EXPORT_DIR; fi
  if [ ! -d $CBP_EXPORT_DIR/river/$scenario_name ] ; then mkdir $CBP_EXPORT_DIR/river/$scenario_name; fi
  if [ ! -d $CBP_EXPORT_DIR/river/$scenario_name/hydr ] ; then mkdir $CBP_EXPORT_DIR/river/$scenario_name/hydr; fi
- if [ ! -d $CBP_EXPORT_DIR/river/$scenario_name/hydr ] ; then mkdir $CBP_EXPORT_DIR/river/$scenario_name/divr; fi
- if [ ! -d $CBP_EXPORT_DIR/river/$scenario_name/hydr ] ; then mkdir $CBP_EXPORT_DIR/river/$scenario_name/ps_flow; fi
+ if [ ! -d $CBP_EXPORT_DIR/river/$scenario_name/divr ] ; then mkdir $CBP_EXPORT_DIR/river/$scenario_name/divr; fi
+ if [ ! -d $CBP_EXPORT_DIR/river/$scenario_name/ps_flow ] ; then mkdir $CBP_EXPORT_DIR/river/$scenario_name/ps_flow; fi
+ if [ ! -d $CBP_EXPORT_DIR/river/$scenario_name/rovol ] ; then mkdir $CBP_EXPORT_DIR/river/$scenario_name/rovol; fi
 
 echo 'CBP_ROOT:' $CBP_ROOT
 echo 'CBP_EXPORT_DIR:' $CBP_EXPORT_DIR
@@ -34,3 +35,16 @@ Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file
 
 Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/export_hsp_h5.R $h5_file_path $output_path/ps_flow/$basin'_psflow.csv' $data_source_ps'/table'
   echo 'ps csv created'
+
+
+# converting ROVOL in hydr csv to cfs 
+## can be later modified within the Rscript to convert ps_flow and divr to mgd!
+
+Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/hsp_hydr_conversion.R $basin $scenario_name $output_path/hydr/$basin'_hydr.csv'
+  echo 'Qout converted to cfs in hydr.csv'
+
+# exporting a csv in the wdm format from hydr (year, month, day, hour, [wanted col])
+
+Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/csv_export_wdm_format.R $basin'_hydr.csv' $basin'_rovol.csv' $'Qout'
+  echo 'wdm format csv with ROVOL exported'
+
