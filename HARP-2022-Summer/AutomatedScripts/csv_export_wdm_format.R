8cf663e48f8c947b25c4a831537cc73f98049af7
unknown <juliasb@vt.edu>
2022-08-12 13:58:51 -0400

    edited wdm format script file

--- HARP-2022-Summer/AutomatedScripts/csv_export_wdm_format.R
+++ HARP-2022-Summer/AutomatedScripts/csv_export_wdm_format.R
@@ -17,14 +17,19 @@ omsite = "http://deq1.bse.vt.edu:81"
 #hydr <- fread("OR1_7700_7980_hydr.csv") #for testing
 
 argst <- commandArgs(trailingOnly = T)
-river_seg <- argst[1]
-#river_seg <- ('OR1_7700_7980')
-file_path <- argst[2]
-#file_path <- ('/media/model/p532/out/river/hsp2_2022')
+hydr_file <- argst[1]
+#hydr_file <- ('OR1_7700_7980_hydr.csv')
+rovol_file <- argst[2]
+#rovol_file <- ('OR1_7700_7980_rovol.csv')
+column <- argst[3]
+#column <- ('Qout')
 
-hydr_file=paste(file_path,'/hydr/', river_seg, '_hydr.csv', sep = '')
+hydr_path=paste('/media/model/p532/out/river/hsp2_2022/hydr/', hydr_file, sep = '')
+rovol_path=paste('/media/model/p532/out/river/hsp2_2022/rovol/', rovol_file, sep = '')
 
-hydr <- fread(hydr_file)
+hydr <- fread(hydr_path)
+
+hydr %>% select(column) -> hydr_column
 
 # the hydr.csv contains the year and month columns already
 hydr$day <- day(hydr$date)
@@ -36,20 +41,10 @@ month_comma <- paste0(as.character(hydr$month), ',')
 day_comma <- paste0(as.character(hydr$day), ',')
 hour_comma <- paste0(as.character(hydr$hour), ',')
 
-ovol3_cfs <- hydr$OVOL3*12.1 # ovol had to be converted to cfs from ac.ft/hr
-
 # creating tables with OVOL3 and ROVOL
-hydr_ovol3 <- data.frame(year_comma, month_comma, day_comma, hour_comma, ovol3_cfs)
-colnames(hydr_ovol3) <- c('year', 'month', 'day', 'hour', 'OVOL3')
-
-hydr_rovol <- data.frame(year_comma, month_comma, day_comma, hour_comma, hydr$Qout)
-colnames(hydr_rovol) <- c('year', 'month', 'day', 'hour', 'ROVOL')
+hydr_df <- data.frame(year_comma, month_comma, day_comma, hour_comma, hydr_column)
 
 # exporting the tables
-output_file_ovol3=paste(file_path,'/fortran/', river_seg, '_ovol3.csv', sep = '')
-output_file_rovol=paste(file_path,'/fortran/', river_seg, '_rovol.csv', sep = '')
-
-write.table(hydr_ovol3, file = output_file_ovol3, sep = ",", row.names = FALSE)
-write.table(hydr_rovol, file = output_file_rovol, sep = ",", row.names = FALSE)
+write.table(hydr_df, file = rovol_path, sep = ",", row.names = FALSE, col.names = FALSE)
 
 
