0d5716857b7bb224bfcecc48044668120762bd6e
glenncampagna <gcampagna11@vt.edu>
2022-09-29 12:24:50 -0400

    sub new_col, rm join_col

--- HARP-2022-Summer/join_col.R
+++ HARP-2022-Summer/join_col.R
@@ -32,10 +32,10 @@ df2 <- fread(csv2)
 #df2$month <- month(df2$index)
 #df2$year <- year(df2$index)
 
-names(df2)[names(df2) == old_col] <- 'join_col' #Renames column of interest to join_col for sqldf function 
+#names(df2)[names(df2) == old_col] <- 'join_col' #Renames column of interest to join_col for sqldf function 
 
 df1 <- sqldf(
-  paste0("select a.*, b.", old_col, " as ", join_col, 
+  paste0("select a.*, b.", old_col, " as ", new_col, 
          "from df1 as a left outer join df2 as b
          on (  
          a.year = b.year
@@ -45,4 +45,4 @@ df1 <- sqldf(
          )
   )
 
-names(df1)[names(df1) == 'join_col'] <- new_col #Renames joined column to what is given as an argument
+#names(df1)[names(df1) == 'join_col'] <- new_col #Renames joined column to what is given as an argument
