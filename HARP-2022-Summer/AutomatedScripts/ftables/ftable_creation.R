f83c859dd6e60049d46ea76be8bd2b53265060f0
megpritch <104520247+megpritch@users.noreply.github.com>
2022-10-04 19:49:55 -0400

    updated ftable creation

--- HARP-2022-Summer/AutomatedScripts/ftables/ftable_creation.R
+++ HARP-2022-Summer/AutomatedScripts/ftables/ftable_creation.R
@@ -21,23 +21,63 @@ riverseg <- "JL2_6850_6890"
 channel<- '0. River Channel'
 
 #----Pulling from VAHydro----
-hydrocode <- paste("vahydrosw_wshed",riverseg,sep = "_")
+rseg<- RomFeature$new(ds,list(
+    hydrocode= paste("vahydrosw_wshed",riverseg,sep = "_"), 
+    ftype='vahydro',
+    bundle='watershed'), 
+  TRUE)
 
-feature <- RomFeature$new(ds,list(hydrocode=hydrocode, bundle="watershed",ftype="vahydro"),TRUE)
-model <- RomProperty$new(ds,list(featureid=feature$hydroid, propcode="vahydro-1.0"),TRUE)
+model <- RomProperty$new(ds,list(
+    varkey="om_water_model_node",
+    featureid=rseg$hydroid,
+    entity_type="dh_feature", 
+    propcode="vahydro-1.0"), 
+  TRUE)
 
-#using new json:
-library(jsonlite)
-model_obj_url <- paste(json_obj_url, model$pid, sep="/")
-model_info <- ds$auth_read(model_obj_url, "text/json", "")
-model <- fromJSON(model_info)
-#get values:
-da <- as.numeric(model[[1]][[channel]]$drainage_area$value) #106.052
-prov <- as.numeric(model[[1]][[channel]]$province$value)
-clength <- as.numeric(model[[1]][[channel]]$length$value) #channel length
-cslope <- as.numeric(model[[1]][[channel]]$slope$value) #longitudinal channel slope
+channel_prop <- RomProperty$new(ds,list(
+    varkey="om_USGSChannelGeomObject", #for local_channel it needs _sub added to end
+    featureid=model$pid,
+    entity_type='dh_properties',
+    propname = channel),
+  TRUE)
 
-#----Calculating Channel Geometry----
+drainage_area <- RomProperty$new(channel_prop[["datasource"]],list(
+    varkey="om_class_Constant",
+    featureid=channel_prop$pid,
+    entity_type='dh_properties',
+    propname='drainage_area'),
+  TRUE)
+da <- drainage_area$propvalue
+
+
+province <- RomProperty$new(channel_prop[["datasource"]],list(
+    varkey="om_class_Constant",
+    featureid=channel_prop$pid,
+    entity_type='dh_properties',
+    propname='province'),
+  TRUE)
+prov <- province$propvalue
+
+
+length <- RomProperty$new(channel_prop[["datasource"]],list(
+    varkey="om_class_Constant",
+    featureid=channel_prop$pid,
+    entity_type='dh_properties',
+    propname='length'),
+  TRUE)
+clength <- length$propvalue #channel length
+
+
+slope <- RomProperty$new(channel_prop[["datasource"]],list(
+    varkey="om_class_Constant",
+    featureid=channel_prop$pid,
+    entity_type='dh_properties',
+    propname='slope'),
+  TRUE)
+cslope <-  slope$propvalue #longitudinal channel slope
+
+
+#----Provincial Channel Geometry----
 if (prov == 1){
   #Appalachian Plateau
   hc = 2.030 # "c" = coefficient for regional regression eqn
@@ -86,17 +126,6 @@ if (prov ==4){
   nf = 0.06
 }
 
-if (prov == -1){
-  #Single/General Equation
-  hc = 2.177
-  he = 0.2293
-  bfc = 13.128
-  bfe = 0.4432
-  bc = 5.471
-  be = 0.5103
-  n = 0.037 #avg of ^above n values
-}
-  
 # Regional Regression Eqn's:
 #bank full stage "max height before floodplain":
 h = hc * (da**he)
@@ -109,35 +138,35 @@ z = 0.5 * (bf - b ) / h
 
 
 #----Calculating FTABLE----
-# Depth
-depth <- seq(0,h,length=19) #sequence between zero and bank full stage
-#depth <- c(.000, 0.586, 1.172, 1.758, 2.343, 2.929, 3.515, 4.687, 5.858, 7.030,9.373, 11.717, 14.060, 16.403, 18.747, 21.090, 23.433, 25.777, 28.120)
-#depth <- c(ftable_uci$depth)
+#--additional parameters:
+#depth
+cdepth <- seq(0,h,length=10) #channel
+fdepth <- seq(h+1, h*4 ,length=9) #floodplain
+depth <- c(cdepth, fdepth)
 
-# Surface Area
-    # water surface width * length of channel
-sw <- b + 2*z*depth
-sw[depth == 0] <- 0 # zero depth = zero surface water width
-area = (sw * clength)/43560 #converting to acres
+#floodplain side slope where base = bankfull width & top width = 5x that:
+zf <- 0.5 * (5*bf - bf)/(h) 
 
-# Volume
-    # length * cross sectional area
-vol <- (clength * (0.5*(sw+b)*depth))/43560 #converting to ft-acre
+#--function:
+fn_make_trap_ftable <- function(depth, clength, cslope, b, z, n) { 
+  sw <- b + 2*z*depth #surface width
+  sw[depth == 0] <- 0
+  area <- (sw * clength)/43560 #surface area
+  vol <- (clength * (0.5*(sw+b)*depth))/43560
+  disch <- (1.49/n) * ((depth*(b+z*depth))/(b+2*depth*sqrt(1+z^2)))**(2/3) * cslope**0.5 * 0.5*(sw+b)*depth
+  ftable <- data.frame(depth, area, vol, disch)
+  return(ftable)
+}
 
-# Discharge
-    # Q = V * A ; where A = cross sectional area / flow area
-    # Manning's Eqn: V = (1.49/n) * R^(2/3) * S^(1/2) 
-        # (1.49/n) is English ; (1/n) is metric
-    # Hydraulic Radius = (depth*(b+z*depth))/(b+2*depth*sqrt(1+z^2))
-        # OR Hydraulic R = (depth*(b + sw)/2)/(b + 2*(((sw-b)/2)**2 + depth**2)**0.5)
+cftab <- fn_make_trap_ftable(cdepth, clength, cslope, b, z, n) #in-channel
+fptab <- fn_make_trap_ftable(fdepth-h, clength, cslope, bf, zf, nf) #floodplain
 
-disch <- (1.49/n) * ((depth*(b+z*depth))/(b+2*depth*sqrt(1+z^2)))**(2/3) * 
-  cslope**0.5 * 0.5*(sw+b)*depth
+# add values from below floodplain to fptab
+fptab$depth <- fptab$depth + h
+fptab$vol <- fptab$vol + max(cftab$vol)
+fptab$disch <- fptab$disch + max(cftab$disch)
 
-# Compile 
-ftable <- data.frame(depth, area, vol, disch)
-colnames(ftable) <- c('Depth (ft)', 'Area (acres)', 'Volume (ac-ft)', 'Discharge (cfs)')
-
+ftable_specific <- rbind(cftab, fptab)
 
 #----Exporting----
 
