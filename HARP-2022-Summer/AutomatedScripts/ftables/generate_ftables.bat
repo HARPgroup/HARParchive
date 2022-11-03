9da82bad9ba99aece5a19b7e58cac02b6630b71b
megpritch <104520247+megpritch@users.noreply.github.com>
2022-11-03 11:03:52 -0400

    Update generate_ftables.bat

--- HARP-2022-Summer/AutomatedScripts/ftables/generate_ftables.bat
+++ HARP-2022-Summer/AutomatedScripts/ftables/generate_ftables.bat
@@ -11,16 +11,17 @@ echo 'parameters:' $parameters
 
 # check the directory where ftables will be stored
 if [ ! -d $CBP_ROOT/input/param/river/$scenario ] ; then mkdir $CBP_ROOT/input/param/river/$scenario; fi
-if [ ! -d $CBP_ROOT/input/param/river/$scenario/ftables ] ; then mkdir $CBP_ROOT/input/param/river/$scenario/ftables; fi
+if [ ! -d $CBP_ROOT/input/param/river/$scenario/$parameters ] ; then mkdir $CBP_ROOT/input/param/river/$scenario/$parameters; fi
+if [ ! -d $CBP_ROOT/input/param/river/$scenario/$parameters/ftables ] ; then mkdir $CBP_ROOT/input/param/river/$scenario/$parameters/ftables; fi
 
 echo 'CBP_ROOT:' $CBP_ROOT
 
 # running the R script: 
-for %%i in $riverseg_list do
+for i in "${riverseg_list[@]}" do
  
- output_path= $CBP_ROOT/input/param/river/$scenario/ftables/`%%i`.ftable
+output_path= $CBP_ROOT/input/param/river/$scenario/$parameters/ftables/$i.ftable
  
-  ftable= ($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/ftable_creation.R `%%i` $channel $output_path))
+ftable= ($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/ftable_creation.R $i $channel $output_path))
   
 done
 
