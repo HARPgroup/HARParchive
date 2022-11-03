eb5ec7e3633a5d3d078d5f6d7df0310ba99e0e31
megpritch <104520247+megpritch@users.noreply.github.com>
2022-11-03 11:25:07 -0400

    Update generate_ftables.bat

--- HARP-2022-Summer/AutomatedScripts/ftables/generate_ftables.bat
+++ HARP-2022-Summer/AutomatedScripts/ftables/generate_ftables.bat
@@ -24,6 +24,7 @@ for i in "${riverseg_list[@]}" do
 output_path=$CBP_ROOT/input/param/river/$scenario/$parameters/ftables/$i.ftable
  
 ftable=($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/ftable_creation.R $i $channel $output_path))
-  
+echo $i
+
 done
 
