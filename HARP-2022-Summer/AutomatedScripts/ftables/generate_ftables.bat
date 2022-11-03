f809e8fc2d3aba8570a7173566264944121eed93
megpritch <104520247+megpritch@users.noreply.github.com>
2022-11-03 10:24:52 -0400

    Update generate_ftables.bat

--- HARP-2022-Summer/AutomatedScripts/ftables/generate_ftables.bat
+++ HARP-2022-Summer/AutomatedScripts/ftables/generate_ftables.bat
@@ -9,16 +9,16 @@ cd /opt/model/p6/vadeq #or just run batch from this Dir.
 parameters=`cbp get_config vadeq_2021 river PARAMETERS`
 
 # check the directory where ftables will be stored
-if [ ! -d $CBP_ROOT/input/param/river/$scenario] ; then mkdir $CBP_ROOT/input/param/river/$scenario; fi
-if [ ! -d $CBP_ROOT/input/param/river/$scenario/ftables] ; then mkdir $CBP_ROOT/input/param/river/$scenario/ftables; fi
+if [ ! -d $CBP_ROOT/input/param/river/$scenario ] ; then mkdir $CBP_ROOT/input/param/river/$scenario; fi
+if [ ! -d $CBP_ROOT/input/param/river/$scenario/ftables ] ; then mkdir $CBP_ROOT/input/param/river/$scenario/ftables; fi
 
 
 # running the R script: 
 for %%i in $riverseg_list do
  
- output_path= $CBP_ROOT/input/param/river/$scenario/ftables/%%i.ftable
+ output_path= $CBP_ROOT/input/param/river/$scenario/ftables/`%%i`.ftable
  
-  ftable= ($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/ftable_creation.R %%i $channel $output_path))
+  ftable= ($(Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/ftable_creation.R `%%i` $channel $output_path))
   
 done
 
