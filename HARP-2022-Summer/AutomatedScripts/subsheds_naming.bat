dab93309bd955de38d17c980b8ea2bebb0ff5599
unknown <juliasb@vt.edu>
2023-01-09 11:09:24 -0500

    edits to filepaths

--- HARP-2022-Summer/AutomatedScripts/subsheds_naming.bat
+++ HARP-2022-Summer/AutomatedScripts/subsheds_naming.bat
@@ -3,15 +3,15 @@
 
 . hspf_config #Loads version-specific cbp variables
 
-subsheds_path=$1
-	#/HARParchive/HARP-2022-Summer/AutomatedScripts/SubshedsCreation/subshed_riversegs.txt
-master_list=$2
-	#/opt/model/p6/vadeq/config/catalog/geo/vahydro/rivernames.csv
+subsheds_path=$1     #/HARParchive/HARP-2022-Summer/AutomatedScripts/SubshedsCreation/subshed_riversegs.txt
+model_version=$2     #cbp-6.0
 
-dataset=`cbp get_config subsheds river GEO`
-	#results as "vahydro"
-  echo 'dataset from GEO:' $dataset
+dataset=`cbp get_config subsheds river GEO`     #"vahydro"
 
-for /F "tokens=*" %%G in ($subsheds_path) do [Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/subsheds_naming.R %%G $master_list] echo 'subshed %%G was renamed'
+echo 'CBP_ROOT:' $CBP_ROOT
 
-	
+master_list=$CBP_ROOT/config/catalog/geo/$dataset/rivernames.csv     #/opt/model/p6/vadeq/config/catalog/geo/vahydro/rivernames.csv
+  echo 'list of rivernames from:' $master_list
+
+for /F "tokens=*" %%G in ($subsheds_path) do [Rscript ~/HARParchive/HARP-2022-Summer/AutomatedScripts/subsheds_naming.R %%G $master_list $model_version] echo 'subshed %%G was renamed'
+
