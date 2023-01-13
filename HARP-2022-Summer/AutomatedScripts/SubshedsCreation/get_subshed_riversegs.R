6d7d26a1cceca76f986b5d79c11af07e09783f87
megpritch <104520247+megpritch@users.noreply.github.com>
2023-01-13 13:38:54 -0500

    proportion fixed

--- HARP-2022-Summer/AutomatedScripts/SubshedsCreation/get_subshed_riversegs.R
+++ HARP-2022-Summer/AutomatedScripts/SubshedsCreation/get_subshed_riversegs.R
@@ -1,11 +1,4 @@
 
-# Setup
-suppressPackageStartupMessages(library("hydrotools")) #needed to pull values from VAHydro 
-# Link data source (ds) to VAHydro
-basepath='/var/www/R';
-source("/var/www/R/config.R") #will need file in same folder/directory
-ds <- RomDataSource$new(site, rest_uname = rest_uname)
-ds$get_token(rest_pw)
 
 #commands
 argst <- commandArgs(trailingOnly = T)
@@ -13,15 +6,58 @@ path <- argst[1]
 #testing ! comment out later:
 #path <- '/aa_HARP/aa_GitHub/HARParchive/HARP-2022-Summer/AutomatedScripts/SubshedsCreation/'
 
+#retrieve subshed da----
+site <- "http://deq1.bse.vt.edu/d.dh"    #Specify the site of interest, either d.bet OR d.dh
+
+# Load Libraries
+suppressPackageStartupMessages(library("hydrotools")) #needed to pull values from VAHydro 
+basepath='/var/www/R';
+source(paste(basepath,'config.R',sep='/'))
+ds = RomDataSource$new(site, rest_uname)
+ds$get_token(rest_pw)
+
+# get the DA, need to grab a model output first in order to ensure segments with a channel subcomp
+# are included
+
+# GET DA
+df_area <- data.frame(
+  'model_version' = c('vahydro-1.0',  'vahydro-1.0',  'vahydro-1.0'),
+  'runid' = c('runid_11', '0.%20River%20Channel', 'local_channel'),
+  'runlabel' = c('QBaseline_2020', 'comp_da', 'subcomp_da'),
+  'metric' = c('Qbaseline', 'drainage_area', 'drainage_area')
+)
+da_data <- om_vahydro_metric_grid(
+  metric = metric, runids = df_area,
+  base_url = paste(site,'entity-model-prop-level-export',sep="/"),
+  ds = ds
+)
+da_data <- sqldf(
+  "select pid, comp_da, subcomp_da, riverseg,
+   CASE
+    WHEN comp_da is null then subcomp_da
+    ELSE comp_da
+    END as da
+   from da_data
+  ")
+
+#isolate da for our subshed:
+vahydro_subs <- subset(da_data, nchar(da_data$riverseg)>13)
+vahydro_subs <- vahydro_subs[,-2:-3]
+#vahydro_sub <- vahydro_subs[grep(main_seg, vahydro_subs$riverseg), ]
+
+file <- paste(path, 'subshed_list.csv', sep='')
+write.table(vahydro_subs, file = file, quote = FALSE, row.names = FALSE, col.names = TRUE, sep=",")
+
+#-old----
 #identify subsheds by a name longer than 29 characters
-rsegs <- RomFeature$new(ds,list(ftype='vahydro',bundle='watershed'),TRUE)
-subshed_hydrocodes <- subset(rsegs$hydrocode, nchar(rsegs$hydrocode)>29)
-subshed_hydrocodes <- data.frame(subshed_hydrocodes) #collect hydrocodes into data frame
+#rsegs <- RomFeature$new(ds,list(ftype='vahydro',bundle='watershed'),TRUE)
+#subshed_hydrocodes <- subset(rsegs$hydrocode, nchar(rsegs$hydrocode)>29)
+#subshed_hydrocodes <- data.frame(subshed_hydrocodes) #collect hydrocodes into data frame
 
 #make hydrocodes into riverseg's only (for ftable_creation purposes)
-subshed_riversegs <- gsub("vahydrosw_wshed_","",as.character(subshed_hydrocodes$subshed_hydrocodes))
-subshed_riversegs <- data.frame(subshed_riversegs)
+#subshed_riversegs <- gsub("vahydrosw_wshed_","",as.character(subshed_hydrocodes$subshed_hydrocodes))
+#subshed_riversegs <- data.frame(subshed_riversegs)
 
 #write to csv
-file <- paste(path, 'subshed_riversegs.txt', sep='')
-write.table(subshed_riversegs, file = file, quote = FALSE, row.names = FALSE, col.names = FALSE)
+#file <- paste(path, 'subshed_riversegs.txt', sep='')
+#write.table(subshed_riversegs, file = file, quote = FALSE, row.names = FALSE, col.names = FALSE)
--- HARP-2022-Summer/AutomatedScripts/SubshedsCreation/proportion_landuse.R
+++ HARP-2022-Summer/AutomatedScripts/SubshedsCreation/proportion_landuse.R
@@ -1,9 +1,6 @@
 #proportioning landuses of multiple land segments 
 #when adding a new subshed to a river segment
-
 suppressPackageStartupMessages(library("sqldf"))
-suppressPackageStartupMessages(library("hydrotools"))#needed to pull values from VAHydro 
-
 #----
 argst <- commandArgs(trailingOnly = T)
 main_seg <- argst[1]
@@ -12,9 +9,10 @@ area <- argst[3]
 file <- argst[4]
 
 #testing:----
-#main_seg <- 'PS2_5560_5100'
-#subshed <- 'PS2_5568_5560'
+main_seg <- 'PS2_5560_5100'
+subshed <- 'PS2_5568_5560'
 #main_seg <- 'OD3_8720_8900'
+#main_seg <- 'JB0_7050_0000'
 #subshed <- 'OD3_8723_8720'
 
 # file <- '/opt/model/p6/vadeq/input/scenario/river/land_use/land_use_2013VAHYDRO2018615.csv'
@@ -22,55 +20,13 @@ file <- argst[4]
 # setwd("/Users/VT_SA/Documents/HARP") # for testing only
 # landuse_full <- read.csv('land_use_2013VAHYDRO2018615.csv', sep =',')
 
-#retrieve subshed da----
-site <- "http://deq1.bse.vt.edu/d.dh"    #Specify the site of interest, either d.bet OR d.dh
-
-# Load Libraries
-basepath='/var/www/R';
-source(paste(basepath,'config.R',sep='/'))
-ds = RomDataSource$new(site, rest_uname)
-ds$get_token(rest_pw)
-
-# get the DA, need to grab a model output first in order to ensure segments with a channel subcomp
-# are included
-
-#token <- 'wrRjwZK6jtr975TbkEmwxBtEbW3ff_ICJ5PzMEFsrzM'
-
-# GET DA
-df_area <- data.frame(
-  'model_version' = c('vahydro-1.0',  'vahydro-1.0',  'vahydro-1.0'),
-  'runid' = c('runid_11', '0.%20River%20Channel', 'local_channel'),
-  'runlabel' = c('QBaseline_2020', 'comp_da', 'subcomp_da'),
-  'metric' = c('Qbaseline', 'drainage_area', 'drainage_area')
-)
-da_data <- om_vahydro_metric_grid(
-  metric = metric, runids = df_area,
-  base_url = paste(site,'entity-model-prop-level-export',sep="/"),
-  ds = ds
-)
-da_data <- sqldf(
-  "select pid, comp_da, subcomp_da, riverseg,
-   CASE
-    WHEN comp_da is null then subcomp_da
-    ELSE comp_da
-    END as da
-   from da_data
-  ")
-
-#isolate da for our subshed:
-vahydro_subs <- subset(da_data, nchar(da_data$riverseg)>13)
-vahydro_sub <- vahydro_subs[grep(main_seg, vahydro_subs$riverseg), ]
-da <- vahydro_sub$da
-
-#extract/calculate main ws and subshed areas and calculate their proportion:----
+#-extract/calculate main ws and subshed areas and calculate their proportion:----
 landuse_full <- read.csv(file, sep=',')
 
 receiving_landuses <- sqldf(paste0("select * from landuse_full where riverseg = '",main_seg,"'"))
 mainws_area <- sum(receiving_landuses[-1:-2])/640 #calculate the main ws total area
 
 subsheds <- sqldf(paste0("select * from landuse_full where riverseg = '",subshed,"'"))
-
-#subsheds$landseg <- "N51165" #take this out later
 
 #if subshed has no data, make it a data frame of zeros--
 empty <- data.frame(matrix(0, 0,length(colnames(receiving_landuses))))
@@ -81,58 +37,39 @@ if (empty %in% subsheds) {
   colnames(subsheds) <- colnames(receiving_landuses)
 }
 #--
-
 sub_area <- sum(subsheds[-1:-2])/640 #convert to sq mi
 
 #calculating the proportioning between main ws and subshed:
 propor <- da/mainws_area
 
-#check if subtraction/addition has already been performed; if not, proceed to calculate new subsheds:----
-if (da - da*0.05 <= sub_area & sub_area <= da + da*0.05) {
-  message('Main river segment and subsheds have already been proportioned.')
-  q("n") #ENDING 1
-}
-
-lsegs <- receiving_landuses[-1:-1]
-#lsegs_revert <- data.frame(matrix(0, 0, length(colnames(lsegs))))
-lsegs_revert <- data.frame(matrix(0, length(lsegs$landseg), length(colnames(lsegs))))
-#lsegs_revert <- data.frame()
-colnames(lsegs_revert) <- colnames(lsegs)
-lsegs_revert$landseg <- lsegs$landseg
-
-if (sub_area != 0) {
-  # add subshed areas back into main riverseg as fail safe:
- 
-  for (i in 1:length(subsheds$landseg)) {
-    lseg_sort <- sqldf(paste0("select * from lsegs where landseg = '", subsheds$landseg[i], "'"))
-    lsegs_new <- lseg_sort[i, -1] + subsheds[i, -1:-2]
-    lsegs_new <- cbind(subsheds$landseg[i], lsegs_new)
-    colnames(lsegs_new) <- colnames(lsegs)
-    #rows <- grep(subsheds$landseg[i], lsegs_revert$landseg)
-    lsegs_revert <- rbind(lsegs_revert, lsegs_new)
+#--if existing subshed values are incorrect, add them back to main_seg to be re-proportioned--
+if (sub_area != 0) { # add subshed areas back into main riverseg as fail safe
+  
+  for (i in 1:length(receiving_landuses$landseg)) {
+    #for each landuse in subshed, find the corresponding data in receiving_landuses
+    lseg_subshed <- sqldf(paste0("select * from subsheds where landseg = '", receiving_landuses$landseg[i], "'"))
+    
+    lseg_receive <- sqldf(paste0("select * from receiving_landuses where landseg = '", receiving_landuses$landseg[i], "'"))
+    
+    if  (!(empty %in% lseg_subshed)) {
+      receiving_landuses[-1:-2] <- lseg_subshed[-1:-2] + lseg_receive[-1:-2]
+    }
+    
   }
-  landuse <- lsegs_revert[-1:-1]
-  
-} else {    # if no previous subshed data - they have to be created for each lseg
-  landuse <- receiving_landuses[-1:-2]
-  lsegs_revert <- data.frame(matrix(0, length(lsegs$landseg), length(colnames(lsegs))))
-  colnames(lsegs_revert) <- colnames(lsegs)
 }
 
-# create or re-create subsheds and remove from main seg:
+#-calculate subsheds and remove from main seg----
+new_landuses <- receiving_landuses #new as in creating subshed
+new_landuses[-1:-2] <- new_landuses[-1:-2] * propor
+new_landuses$riverseg <- subshed
 
-sub_inc <- landuse * propor #add the proportion to the new subshed land uses
-new_subshed <- cbind(subshed, lsegs_revert[1], sub_inc)
-colnames(new_subshed) <- colnames(landuse_full)
-  
-lseg_dec <- (landuse - sub_inc) #subtract the proportion from main river segment
-new_rseg <- cbind(receiving_landuses[1], lsegs_revert[1], lseg_dec)
-colnames(new_rseg) <- colnames(landuse_full)
-  
+receiving_landuses[-1:-2] <- receiving_landuses[-1:-2] * (1-propor) #to "subtract" propor from main_seg
+
 remove <- subset(landuse_full, riverseg != main_seg) #remove old land use values
-new_list <- rbind(remove, new_rseg, new_subshed)
-new_list <- new_list[order(new_list$landseg, new_list$riverseg),] #order new list by land seg first and then river seg
- 
+new_list <- rbind(remove, new_landuses, receiving_landuses) #adding in revisions
+new_list <- new_list[order(new_list$landseg, new_list$riverseg),] #order by land seg first, then river seg
+
+
 write.table(new_list,
             file=file,
             append = FALSE,
