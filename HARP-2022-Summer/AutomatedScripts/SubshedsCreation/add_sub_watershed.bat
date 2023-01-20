608badd130845e9dd9ff3b9f1ca05f92e9d5b2e2
Robert W. Burgholzer <rburghol@vt.edu>
2023-01-20 21:05:07 +0000

    updated land use block

--- HARP-2022-Summer/AutomatedScripts/SubshedsCreation/add_sub_watershed.bat
+++ HARP-2022-Summer/AutomatedScripts/SubshedsCreation/add_sub_watershed.bat
@@ -1,9 +1,15 @@
+#!/bin/bash
+if [ $# -lt 5 ]; then
+  echo "Usage: add_sub_watershed.bat hydrocode downstream_seg model_version scenario drainage_area"
+  exit
+fi
+
 # arguments:
 # - hydrocode
 # - receiving stream segment
-# - drainage area of subshed
 # - model version (cbp-6.0)
 # - cbp scenario (subsheds)
+# - drainage area of subshed
 # load name, rseg and area info
 
 hydrocode=$1
@@ -16,37 +22,44 @@ darea=$5
 GEO=`cbp get_config subsheds river GEO`
 
 # name subshed or retrieve the name if it already exists
-read -r subshed downstream <<< "$(Rscript /opt/model/p6/vadeq/run/resegment/subsheds_naming.R $hydrocode /opt/model/p6/vadeq/config/catalog/${GEO}/vahydro/rivernames.csv cbp-6.0)"
+read -r subshed downstream <<< "$(Rscript $CBP_MODEL_ROOT/run/resegment/subsheds_naming.R $hydrocode $CBP_MODEL_ROOT/config/catalog/${GEO}/vahydro/rivernames.csv cbp-6.0)"
   echo'new subshed:' $subshed
 
 # set and proportion watershed area
-Rscript /opt/model/p6/vadeq/run/resegment/area_propor.R /opt/model/p6/vadeq/config/catalog/${GEO}/vahydro/land_water_area.csv $subshed $downstream $darea
+# temp commented out waiting for fix
+#Rscript $CBP_MODEL_ROOT/run/resegment/area_propor.R $CBP_MODEL_ROOT/config/catalog/geo/${GEO}/land_water_area.csv $subshed $downstream $darea
+# use in place temporarily, note argument order is different
+Rscript $CBP_MODEL_ROOT/run/resegment/area_propor.R $downstream $subshed $darea $CBP_MODEL_ROOT/config/catalog/geo/${GEO}/land_water_area.csv 
   echo'land_water_area.csv proportioned'
 
 # set land use area; iterate through multiple files and proportion them all
 cnt=0
 for i in $LANDUSE; do
   ((cnt++))
-  if [ $cnt eq 4 ]; then
-    lu_file="land_use_${i}.csv"
-    echo $lu_file
-    Rscript /opt/model/p6/vadeq/run/resegment/area_propor.R /opt/model/p6/vadeq/config/catalog/${GEO}/vahydro/$lu_file $subshed $downstream $darea
+  if [[ $cnt -eq 1 ]]; then
+    yr=$i
+  fi
+  if [[ $cnt -eq 4 ]]; then
+    lu_file="input/scenario/river/land_use/land_use_${i}.csv"
+    echo "LU file for $yr = " $lu_file
+    Rscript $CBP_MODEL_ROOT/run/resegment/area_propor.R $lu_file $subshed $downstream $darea
+
     # reset our counter
     cnt=0
   fi
-end
-  echo'land use files proportioned'
+done
+echo'land use files proportioned'
 
 # duplicate information from downstream to new subshed
-Rscript /opt/model/p6/vadeq/run/resegment/copy_parent.R /opt/model/p6/vadeq/input/param/transport/wF180615RXAPXXXW_l2w.csv $subshed $downstream
+Rscript $CBP_MODEL_ROOT/run/resegment/copy_parent.R $CBP_MODEL_ROOT/input/param/transport/wF180615RXAPXXXW_l2w.csv $subshed $downstream
   echo'wF180615RXAPXXXW_l2w.csv duplicated'
    # How many transport files need to be duplicated - do we need a loop like above, or just run them one-by-one?
 
-Rscript /opt/model/p6/vadeq/run/resegment/copy_parent.R /opt/model/p6/vadeq/input/param/river/vahydro_2022/gen_info_rseg.csv $subshed $downstream
+Rscript $CBP_MODEL_ROOT/run/resegment/copy_parent.R $CBP_MODEL_ROOT/input/param/river/vahydro_2022/gen_info_rseg.csv $subshed $downstream
  echo'gen_info_rseg.csv duplicated'
 
-Rscript /opt/model/p6/vadeq/run/resegment/copy_parent.R /opt/model/p6/vadeq/config/catalog/${GEO}/vahydro/river_met_wdm.csv $subshed $downstream
+Rscript $CBP_MODEL_ROOT/run/resegment/copy_parent.R $CBP_MODEL_ROOT/config/catalog/${GEO}/vahydro/river_met_wdm.csv $subshed $downstream
  echo'river_met_wdm.csv duplicated'
 
-Rscript /opt/model/p6/vadeq/run/resegment/copy_parent.R /opt/model/p6/vadeq/config/catalog/${GEO}/vahydro/river_prad_wdm.csv $subshed $downstream
+Rscript $CBP_MODEL_ROOT/run/resegment/copy_parent.R $CBP_MODEL_ROOT/config/catalog/${GEO}/vahydro/river_prad_wdm.csv $subshed $downstream
  echo'river_prad_wdm.csv duplicated'
