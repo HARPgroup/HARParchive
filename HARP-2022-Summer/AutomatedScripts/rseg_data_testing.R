2da0cf27a58d185f4b36c2571aa775f8e081113e
unknown <juliasb@vt.edu>
2023-04-10 16:31:55 -0400

    changes

--- HARP-2022-Summer/AutomatedScripts/rseg_data_testing.R
+++ HARP-2022-Summer/AutomatedScripts/rseg_data_testing.R
@@ -4,19 +4,22 @@ library(readr)
 rseg.data.1 <- data.table::fread('JA4_7280_7340_hydrd_wy.csv')
 rseg.data.2 <- data.table::fread('JA4_7280_7340_hydrd_wy_sub.csv')
 
-sdate <- as.Date(min(rseg.data.1$date))         # set as 1 - since we want the same date range for all models(?)
-edate <- as.Date(max(rseg.data.1$date))
+data.match <- match(rseg.data.1$date, rseg.data.2$date)
+sdate <- as.Date(min(data.match))
+edate <- as.Date(max(data.match))
 
-rseg.cols.1 <- names(rseg.data.1)
-#rseg.flow.1 <- zoo(as.numeric(as.character( rseg.data.1$Qout )), order.by = rseg.data.1$index)
-#rseg.flow.1 <- window(rseg.flow.1, start = sdate, end = edate)
-rseg.data.1 <- zoo(rseg.data.1, order.by = rseg.data.1$index)
-rseg.data.1 <- window(rseg.data.1, start = sdate, end = edate)
-mode(rseg.data.1) <- 'numeric'
+rseg.flow.1 <- zoo(as.numeric(as.character(rseg.data.1$Qout)), order.by = rseg.data.1$index)
+rseg.flow.1 <- window(rseg.flow.1, start = sdate, end = edate)
+mode(rseg.flow.1) <- 'numeric'
 
-rseg.cols.2 <- names(rseg.data.2)
-#rseg.flow.2 <- zoo(as.numeric(as.character( rseg.data.2$Qout )), order.by = rseg.data.2$date)
-#rseg.flow.2 <- window(rseg.flow.2, start = sdate, end = edate)
-rseg.data.2 <- zoo(rseg.data.2, order.by = rseg.data.2$index)
-rseg.data.2 <- window(rseg.data.2, start = sdate, end = edate)
-mode(rseg.data.2) <- 'numeric'
+rseg.flow.2 <- zoo(as.numeric(as.character(rseg.data.2$Qout)), order.by = rseg.data.2$index)
+rseg.flow.2 <- window(rseg.flow.2, start = sdate, end = edate)
+mode(rseg.flow.2) <- 'numeric'
+
+
+
+# rseg.file.path <- c("/media/model/p6/out/river/hsp2_2022/hydr/JA4_7280_7340_hydrd_wy.csv", "/media/model/p6/out/river/subsheds/hydr/JA4_7280_7340_hydrd_wy.csv" )
+# rseg.hydrocode <- c("JA4_7280_7340","vahydrosw_wshed_JA4_7280_7340" )
+# rseg.ftype <- c("cbp60","vahydro" )
+# rseg.model.version <- c("cbp-6.0","cbp-6.1")
+# runid.list <- c("hsp2_2022","subsheds")
