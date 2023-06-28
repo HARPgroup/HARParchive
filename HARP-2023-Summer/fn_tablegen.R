a6b62a4a75f1d292be0624ac9fb52ce0bc1e0f68
EllaF21 <ellaf21@vt.edu>
2023-06-28 14:29:29 -0400

    6/28 updates

---
+++ HARP-2023-Summer/fn_tablegen.R
@@ -0,0 +1,36 @@
+## Establishing a function to generate table when given data 
+# Loading required libraries for table
+library(flextable)
+
+#create function
+fn_tablegen <- function(tabledf) { 
+  
+#set table defaults of theme and font and make sure theres no blanks from nas 
+set_flextable_defaults(
+  font.size = 10, theme_fun = theme_zebra, 
+  padding = 6, 
+  na_str = "NA", nan_str = "NA")
+  
+  
+#alignment of table
+table %>%
+  autofit() %>%
+  flextable::align(align = "center", j = c(1:9),part = "all") 
+
+tabledf <- flextable(tabledf)
+
+#change titles of columns in table
+#not needed if declared in data frame
+# table <- set_header_labels(table,
+#                            Facility = "Facility Name", 
+#                            NUM = "Facility Number", 
+#                            Source_Type = "Source Type",
+#                            Rivseg= "River Segment", 
+#                            rivsegName = " River Segment Names",
+#                            Locality = "Location(County)",
+#                            Upstream_Downstream = "Upstream Vs. Downstream", 
+#                            vwp_max_mgy = "Permitted Capacity(mgy)",
+#                            fiveYr = "5 Year Average Use - Source(mgy)"
+                           
+#)
+}
