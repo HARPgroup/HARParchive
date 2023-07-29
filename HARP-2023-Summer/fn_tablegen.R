7fea272d322e438301868f65a326b32985219457
glenncampagna <gcampagna11@vt.edu>
2023-07-28 20:22:39 -0400

    order rivseg table

--- HARP-2023-Summer/fn_tablegen.R
+++ HARP-2023-Summer/fn_tablegen.R
@@ -18,34 +18,20 @@ set_flextable_defaults(
 
 # specify aesthetics for spacing in the table, can be edited if desired
 ft <- flextable(table)
+ft <- theme_vanilla(ft)
 
 if (type == 'facility') {
   ft <- void(ft, j=1, part = "header") #remove name of 1st column in facil/source tables, which will always be the # for the facil/source
   ft <- width(ft, j= 'River Segment ID', width = 1) #making sure rseg ID isn't cut off in facil/source tables
-
-
-ft <- line_spacing(ft, space = 1.25)
-ft <- theme_vanilla(ft)
-ft <- width(ft, j=1, width = 0.25)
-ft <- width(ft, j=2, width = 0.8)
-ft <- align(ft, align = alignment, part = "all")
-  
-
-assign('ft', ft, envir = globalenv())
-}
-
-else if (type == 'riverseg'){
-  
-  ft <- theme_vanilla(ft)
+  ft <- line_spacing(ft, space = 1.25)
+  ft <- width(ft, j=1, width = 0.25)
+  ft <- width(ft, j=2, width = 0.8)
+  ft <- align(ft, align = alignment, part = "all")
+} else if (type == 'riverseg') {
   ft <- autofit(ft)
-  x <- 10 # change percent desired
-  # highlight when percent difference is above x or below negative x 
-  ft <- flextable::highlight(ft, 
-                             #j= "% Difference", 
-                             i = ft$body$dataset$`% Difference` > x |   ft$body$dataset$`% Difference` < -x ,
-                             color = "yellow")
-  
-  assign('ft', ft, envir = globalenv())
-}
+  x <- -10 # change percent desired -- to be moved to config 
+  ft <- flextable::highlight(ft, i = ft$body$dataset$`% Difference` < x ,color = "yellow")  #highlight row when percent difference is below x
+  }
+assign('ft', ft, envir = globalenv())
 }
 
