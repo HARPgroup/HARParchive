a4215e28ad46648c67bf476383856e7f123a1591
glenncampagna <gcampagna11@vt.edu>
2023-07-29 08:34:28 -0400

    display 2nd table from loop with knit_print

--- HARP-2023-Summer/fn_tablegen.R
+++ HARP-2023-Summer/fn_tablegen.R
@@ -12,7 +12,7 @@ fn_tablegen <- function(type, table, alignment) {
 #set table defaults of theme and font and make sure theres no blanks from NAs, can be edited if desired
 set_flextable_defaults(
   font.size = 8, background.color = "white", 
-  padding = 1, na_str = "NA", nan_str = "NA", 
+  na_str = "NA", nan_str = "NA", #padding = 1, 
   table.layout = 'fixed', tabcolsep = 1.5,
   fonts_ignore = T, big.mark = "", decimal.mark= ".") #fonts_ignore applies when knitting as pdf 
 
@@ -23,7 +23,7 @@ ft <- theme_vanilla(ft)
 if (type == 'facility') {
   ft <- void(ft, j=1, part = "header") #remove name of 1st column in facil/source tables, which will always be the # for the facil/source
   ft <- width(ft, j= 'River Segment ID', width = 1) #making sure rseg ID isn't cut off in facil/source tables
-  ft <- line_spacing(ft, space = 1.25)
+#  ft <- line_spacing(ft, space = 1.25)
   ft <- width(ft, j=1, width = 0.25)
   ft <- width(ft, j=2, width = 0.8)
   ft <- align(ft, align = alignment, part = "all")
