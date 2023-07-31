a3000fedc51cbb50f92b19ef096da371b4269734
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2023-07-31 14:59:42 -0400

    one ring to rule them all (all imps)

--- HARP-2023-Summer/get_impoundment_Smin.R
+++ HARP-2023-Summer/get_impoundment_Smin.R
@@ -1,12 +1,14 @@
-# GET VAHydro 1.0 RIVERSEG Smin_pct DATA
+# Get model data attached to an impoundment feature
 df <- data.frame(
   'model_version' = c('vahydro-1.0', 'vahydro-1.0', 'vahydro-1.0', 'vahydro-1.0'),
   'runid' = c('runid_11', 'runid_400', 'runid_600', 'runid_13'),
   'metric' = c('usable_pct_p0','usable_pct_p0', 'usable_pct_p0', 'usable_pct_p0'),
   'runlabel' = c('Smin_pct_2', 'Smin_pct_perm', 'Smin_pct_prop', 'Smin_pct_800')
 )
-wshed_data <- om_vahydro_metric_grid(
-  metric = metric, runids = df, bundle = 'waterbody', ftype = "all",
+
+
+all_imp_data <- om_vahydro_metric_grid(
+  metric = metric, runids = df, bundle = 'all', ftype = "all",
   base_url = paste(site,'entity-model-prop-level-export',sep="/"),
   ds = ds
 )
