81e62549f68e571245c9cb986f8ef925813b7d1c
glenncampagna <gcampagna11@vt.edu>
2023-08-01 10:28:44 -0400

    new params in batch script to test region basin maps

--- HARP-2023-Summer/region_maps_batch.R
+++ HARP-2023-Summer/region_maps_batch.R
@@ -20,16 +20,19 @@ for (i in (1:nrow(region_names))) {
                     output_format = "word_document",
                     params = list(
                       rivseg = "JL7_7070_0001", 
-                      locality = "Montgomery", 
+                      locality = "Stafford", 
                       region = region, 
                       type = "facility", 
                       model_version = "vahydro-1.0", 
-                      runid_list = c("runid_11","runid_13"), 
+                      runid_list_facilities = c("runid_11","runid_13"), 
+                      runid_list_riversegs = c("runid_11","runid_13"),
                       metric_mod = "wd_mgd",
                       metric_feat = "wsp2020_2040_mgy",
+                      rivseg_metric = "l30_Qout",
                       map_type = "region",
+                      map_style = "custom",
                       map_by = "fiveyr_avg_mgy",
-                      limit = "boundary",
+                      limit = "basins",
                       table_col = c("runid_11_wd_mgd","runid_13_wd_mgd","fiveyr_avg_mgy","wsp2020_2040_mgy"),
                       bbox_type = "auto"))
   
