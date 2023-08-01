266ac1f057d0007d9dc7e762a92d013f6d80a139
glenncampagna <gcampagna11@vt.edu>
2023-08-01 12:48:59 -0400

    put colname arg in rollmean calc

--- HARP-2023-Summer/fn_get_pd_min.R
+++ HARP-2023-Summer/fn_get_pd_min.R
@@ -19,7 +19,7 @@ ts_filtered <- as.data.frame(ts_data)
 #need to make sure data for mean is numeric
 class(ts_filtered[,colname]) <- "numeric"
 
-rollmeans <- rollmean(ts_filtered$Flow, k = critical_pd_length) #get all x-day averages using rollmean
+rollmeans <- rollmean(ts_filtered[,colname], k = critical_pd_length) #get all x-day averages using rollmean
 min_pd <- min(rollmeans) #get lowest of all x-day means
     
 return(min_pd)
