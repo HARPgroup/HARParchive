6b48722ac2e12392cf160ea6b3fdd71a4ca5e305
glenncampagna <gcampagna11@vt.edu>
2023-08-02 12:49:13 -0400

    rough draft batch script, currently used just for testing exact vs approx

--- HARP-2023-Summer/update_SminCPL_batch.R
+++ HARP-2023-Summer/update_SminCPL_batch.R
@@ -29,10 +29,18 @@ all_imp_data <- om_vahydro_metric_grid(
   ds = ds
 )
 
-## add loop-through of all elements in all_imp_data
+#all_imp_data$Smin_L90_exact <- NA
+#all_imp_data$Smin_L90_approx <- NA
+
+## add loop-through of all elements in all_imp_data ?
+#for (i in 1:nrow(all_imp_data)) { #fails on i=2
+
+#set i = row for test case of interest
+i = 28
+
 #get model element
-model <- RomProperty$new(ds,list(
-  featureid = all_imp_data$featureid[1],
+model <- RomProperty$new(ds,list( #only works for impoundment features not those within riverseg models 
+  featureid = all_imp_data$featureid[i],
   propcode = "vahydro-1.0",
   varkey = "om_hydroimpoundment"),
   TRUE
@@ -58,8 +66,67 @@ if ("imp_off" %in% cols) {
   imp_off = 0
 }
 
-#should we retrieve L30_year and L90_year from the riverseg-model-scenario here?
-
+#apply Smin_cpl function
 #this would be classified as an exact method, while using sqldf to crop by L30/L90_year would be an approx. method 
-Smin_L90 <- fn_get_pd_min(ts_data = dat, critical_pd_length = 90, date_filter = c('1995-01-01','2020-12-31'), colname = "Storage")
+Smin_L90_exact <- fn_get_pd_min(ts_data = dat, critical_pd_length = 90, date_filter = c('1995-01-01','2020-12-31'), colname = "Storage")
 
+#L30_year and L90_year from the riverseg-model-scenario -- should be in loop
+riverseg<- RomFeature$new( #get riverseg feature from vahydro
+  ds,
+  list(
+    hydrocode=paste('vahydrosw_wshed_',all_imp_data$riverseg[i], sep = ''),
+    ftype='vahydro',
+    bundle='watershed'
+  ),
+  TRUE)
+  
+model <- RomProperty$new( #get vahydro-1.0 model feature from vahydro
+  ds,
+  list(
+    featureid=riverseg$hydroid, 
+    propcode='vahydro-1.0'
+  ), 
+  TRUE)
+  
+model_scenario <- RomProperty$new( #get scenario/runid from vahydro
+  ds,
+  list(
+    varkey="om_scenario", 
+    featureid=model$pid, 
+    propname= paste0('runid_',runid) 
+  ), 
+  TRUE)
+  
+L30prop <- RomProperty$new( #get metric from vahydro
+  ds, list(
+    featureid=model_scenario$pid,
+    entity_type='dh_properties',
+    propname = 'l30_year'
+  ),
+  TRUE)
+L30_year <- L30prop$propvalue
+
+L90prop <- RomProperty$new( #get metric from vahydro
+  ds, list(
+    featureid=model_scenario$pid,
+    entity_type='dh_properties',
+    propname = 'l90_year'
+  ),
+  TRUE)
+L90_year <- L90prop$propvalue
+
+dat_df <- as.data.frame(dat) #zoo to df
+dat_df$L90_year <- L90_year
+
+#Smin using approx method
+Smin_L90 <- sqldf("SELECT min(Storage), year
+                    FROM dat_df 
+                    WHERE year = L90_year")
+
+Smin_L90_approx <- as.numeric(Smin_L90$`min(Storage)`)
+
+#}
+
+paste0(all_imp_data$propname[i])
+paste0('Approx Smin_L90: ', round(Smin_L90_approx, digits = 3))
+paste0('Exact Smin_L90: ', round(Smin_L90_exact, digits = 3))
