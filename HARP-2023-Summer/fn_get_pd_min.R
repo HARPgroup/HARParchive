17b9e1fe4f4ba64602e1de20ff7bb25800f37d4e
glenncampagna <gcampagna11@vt.edu>
2023-08-02 09:54:36 -0400

    fix basin mapping

--- HARP-2023-Summer/fn_get_pd_min.R
+++ HARP-2023-Summer/fn_get_pd_min.R
@@ -3,23 +3,21 @@ library(zoo)
 library(stats)
 
 fn_get_pd_min <- function(ts_data, critical_pd_length, date_filter, colname) {
-
+#timestep in ts_data = unit of critical_pd_length
+  
 #format dates 
 startdate <- as.POSIXct(date_filter[1])
 enddate <- as.POSIXct(date_filter[2])  
 
-#create zoo to filter by date  
-ts_zoo <- zoo(ts_data, order.by = index(ts_data))
+ts_zoo <- zoo(ts_data, order.by = index(ts_data)) #create zoo to filter by date 
 
-#filter by date
-ts_filtered <- window(ts_zoo, start = startdate, end = enddate)
+ts_crop <- window(ts_zoo, start = startdate, end = enddate) #crop by date - stats package
 
-ts_filtered <- as.data.frame(ts_data)
+ts_crop <- as.data.frame(ts_data) #convert to df from zoo
 
-#need to make sure data for mean is numeric
-class(ts_filtered[,colname]) <- "numeric"
+class(ts_crop[,colname]) <- "numeric" #data for rollmean needs to be numeric
 
-rollmeans <- rollmean(ts_filtered[,colname], k = critical_pd_length) #get all x-day averages using rollmean
+rollmeans <- rollmean(ts_crop[,colname], k = critical_pd_length) #get all x-day averages using rollmean
 min_pd <- min(rollmeans) #get lowest of all x-day means
     
 return(min_pd)
