a97beeb979c5fe9ec5ac0cb2cfb34ed9733132ca
glenncampagna <gcampagna11@vt.edu>
2023-08-09 13:00:49 -0400

    source to home for cmnd line use

--- HARP-2023-Summer/update_Smin.R
+++ HARP-2023-Summer/update_Smin.R
@@ -9,8 +9,9 @@ ds$get_token(rest_pw)
 
 # Read args
 argst <- commandArgs(trailingOnly=T)
-runid <- as.integer(argst[1]) #numeric part of a runid (ex. 11)
 pid <- as.integer(argst[2]) #model element pid
+runid <- as.integer(argst[1]) #numeric part of a runid (ex. 11)
+
 
 token = ds$get_token(rest_pw) #needed for elid function 
 elid <- om_get_model_elementid(base_url = site, mid = pid) #get om_element_connection value for elid
@@ -22,12 +23,12 @@ object_class <- RomProperty$new(ds,list(featureid = om_model$pid, propname = 'ob
 #Run 1 of 3 om post-processing scripts based on type of impoundment
 if (object_class=="waterSupplyModelNode") {
   commandArgs <- function(...) c(pid,elid,runid)
-  source(paste0(github_location,"/HARParchive/HARP-2023-Summer/waterSupplyModelNode.R")) #riverseg imp
+  source(paste0("~/HARParchive/HARP-2023-Summer/waterSupplyModelNode.R")) #riverseg imp
 } else if (object_class=="waterSupplyElement") {
   commandArgs <- function(...) c(pid,elid,runid)
-  source(paste0(github_location,"/HARParchive/HARP-2023-Summer/waterSupplyElement.R")) #facility imp
+  source(paste0("~/HARParchive/HARP-2023-Summer/waterSupplyElement.R")) #facility imp
 } else if (object_class=="hydroImpoundment") {
   commandArgs <- function(...) c(pid,elid,runid)
-  source(paste0(github_location,"/HARParchive/HARP-2023-Summer/hydroImpoundment.R")) #standalone imp
+  source(paste0("~/HARParchive/HARP-2023-Summer/hydroImpoundment.R")) #standalone imp
 }
 
