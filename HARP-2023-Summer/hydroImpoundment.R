92d7bb8520663a3884ec4e2c1b30634219367b5f
glenncampagna <gcampagna11@vt.edu>
2023-08-15 14:13:34 -0400

    Dont calc storage if imp not enabled

--- HARP-2023-Summer/hydroImpoundment.R
+++ HARP-2023-Summer/hydroImpoundment.R
@@ -149,41 +149,43 @@ datpd <- window(
   end = l90_end
 );
 
-# Find l30_year for calculation of Smin_L30
-l30 <- loflows["30 Day Min"];
-ndx = which.min(as.numeric(l30[,"30 Day Min"]));
-l30_Qout = round(loflows[ndx,]$"30 Day Min",6);
-l30_year = loflows[ndx,]$"year";
+if (imp_off==0) {
+  # Find l30_year for calculation of Smin_L30
+  l30 <- loflows["30 Day Min"];
+  ndx = which.min(as.numeric(l30[,"30 Day Min"]));
+  l30_Qout = round(loflows[ndx,]$"30 Day Min",6);
+  l30_year = loflows[ndx,]$"year";
+  
+  # Prep for Smin_CPL function
+  start_date_30 <- paste0(l30_year,"-01-01") # Dates for l90_year
+  end_date_30 <- paste0(l30_year,"-12-31")
+  
+  start_date_90 <- paste0(l90_year,"-01-01") # Dates for l30_year
+  end_date_90 <- paste0(l90_year,"-12-31")
+  
+  # Calculate Smin_CPLs using function
+  Smin_L30_acft <- fn_get_pd_min(ts_data = dat, critical_pd_length = 30,
+                            start_date = start_date_30, end_date = end_date_30,
+                            colname = "Storage")
+  
+  Smin_L90_acft <- fn_get_pd_min(ts_data = dat, critical_pd_length = 90,
+                            start_date = start_date_90, end_date = end_date_90,
+                            colname = "Storage")
+  
+  # Convert from from ac-ft to mg: 1 mg = 3.069 acre-feet
+  Smin_L30_mg <- round(Smin_L30_acft/3.069, digits = 3)
+  Smin_L90_mg <- round(Smin_L90_acft/3.069, digits = 3)
+  
+  vahydro_post_metric_to_scenprop(scenprop$pid, 'om_class_Constant', NULL, 'Smin_L30_mg', Smin_L30_mg, ds)
+  vahydro_post_metric_to_scenprop(scenprop$pid, 'om_class_Constant', NULL, 'Smin_L90_mg', Smin_L90_mg, ds)
 
-# Prep for Smin_CPL function
-start_date_30 <- paste0(l30_year,"-01-01") # Dates for l90_year
-end_date_30 <- paste0(l30_year,"-12-31")
-
-start_date_90 <- paste0(l90_year,"-01-01") # Dates for l30_year
-end_date_90 <- paste0(l90_year,"-12-31")
-
-# Calculate Smin_CPLs using function
-Smin_L30_acft <- fn_get_pd_min(ts_data = dat, critical_pd_length = 30,
-                          start_date = start_date_30, end_date = end_date_30,
-                          colname = "Storage")
-
-Smin_L90_acft <- fn_get_pd_min(ts_data = dat, critical_pd_length = 90,
-                          start_date = start_date_90, end_date = end_date_90,
-                          colname = "Storage")
-
-# Convert from from ac-ft to mg: 1 mg = 3.069 acre-feet
-Smin_L30_mg <- round(Smin_L30_acft/3.069, digits = 3)
-Smin_L90_mg <- round(Smin_L90_acft/3.069, digits = 3)
-
-# Set Smin metrics to 0 if impoundment is not active
-if (imp_off == 1) { 
+ } else if (imp_off == 1) { # Set Smin metrics to 0 if impoundment is not active
   Smin_L30_mg <- 0
   Smin_L90_mg <- 0
+  
+  vahydro_post_metric_to_scenprop(scenprop$pid, 'om_class_Constant', NULL, 'Smin_L30_mg', Smin_L30_mg, ds)
+  vahydro_post_metric_to_scenprop(scenprop$pid, 'om_class_Constant', NULL, 'Smin_L90_mg', Smin_L90_mg, ds)
 }
-
-# Post Smin metrics to vahydro
-vahydro_post_metric_to_scenprop(scenprop$pid, 'om_class_Constant', NULL, 'Smin_L30_mg', Smin_L30_mg, ds)
-vahydro_post_metric_to_scenprop(scenprop$pid, 'om_class_Constant', NULL, 'Smin_L90_mg', Smin_L90_mg, ds)
 
 # Elevation periods
 
