ea42773ee27450d12bee18b993ea3b7e199e3a93
glenncampagna <gcampagna11@vt.edu>
2023-12-28 11:11:10 -0500

    pull basline data, join to demand scen and storage data

--- HARP-2023-Summer/WA_methods.R
+++ HARP-2023-Summer/WA_methods.R
@@ -2,7 +2,7 @@
 
 # Example equation: For a L90 scenario:
 # Qdemand = l90_Qout(cfs)
-# Qbaseline (approximation when baseline scenario unavailable) = l90_Qout + wd_mgd - ps_mgd 
+# Qbaseline: either l90_Qout for runid_0, or when baseline scenario (runid_0) unavailable = l90_Qout + wd_mgd - ps_mgd 
 # WA = Qdem - 0.9*Qbase + Smin/CPL = l90_Qout - 0.9*(l90_Qout + wd_mgd - ps_mgd) + Smin_perday (final units should be mgd)
 
 ## Setup
@@ -16,10 +16,15 @@ ds <- RomDataSource$new(site, rest_uname)
 ds$get_token(rest_pw)
 ##
 
-## This will ulttimately be user-controlled:
+## This will ultimately be user-controlled:
 runid <- 11
 ##
 runlabel <- paste0('runid_', runid)
+
+###--- Pull Data From VAhydro ---###
+
+#note: a single om_vahydro_metric_grid() would be sufficient and more concise for 
+# getting all needed metrics as long as the input df is formatted correctly 
 
 #Pulling in Smin metrics (exported using fn_get_pd_min(), which uses the Smin approx. method):
 df_storage <- data.frame(
@@ -34,19 +39,25 @@ storage_data <- om_vahydro_metric_grid(
   ds = ds
 )
 
-#Convert storage from mg to mgd 
-##This should ultimately be user-controlled and not hard-coded
-storage_data$SminL30mgd_11 <- storage_data$SminL30mg_11 / 30 #dividing by # of days in low flow period 
-storage_data$SminL30mgd_13 <- storage_data$SminL30mg_13 / 30
-storage_data$SminL90mgd_11 <- storage_data$SminL90mg_11 / 30
-storage_data$SminL90mgd_13 <- storage_data$SminL90mg_13 / 30
+#Pulling baseline scenario low-flow metrics (runid_0)
+df_baseline <- data.frame(
+  'model_version' = c('vahydro-1.0', 'vahydro-1.0'),
+  'runid' = c('runid_0', 'runid_0'),
+  'metric' = c('l90_Qout', 'l30_Qout'),
+  'runlabel' = c('l90_Qout_base', 'l30_Qout_base')
+)
+baseline_data <- om_vahydro_metric_grid(
+  metric = metric, runids = df_baseline, bundle = 'all', ftype = "all",
+  base_url = paste(site,'entity-model-prop-level-export',sep="/"),
+  ds = ds
+)
 
-#Getting other metrics used in WA equation
+#Pulling demand scenario metrics used in WA equation
 df_metrics <- data.frame(
-  'model_version' = c('vahydro-1.0', 'vahydro-1.0', 'vahydro-1.0', 'vahydro-1.0','vahydro-1.0','vahydro-1.0'),
-  'runid' = c(runlabel, runlabel, runlabel, runlabel, runlabel, runlabel),
-  'metric' = c('l90_Qout', 'l30_Qout', 'l90_year', 'l30_year', 'wd_cumulative_mgd','ps_cumulative_mgd'),
-  'runlabel' = c('l90_Qout', 'l30_Qout', 'l90_year', 'l30_year','wd_cumulative_mgd', 'ps_cumulative_mgd')
+  'model_version' = c('vahydro-1.0', 'vahydro-1.0', 'vahydro-1.0', 'vahydro-1.0'),
+  'runid' = c(runlabel, runlabel, runlabel, runlabel),
+  'metric' = c('l90_Qout', 'l30_Qout', 'wd_cumulative_mgd','ps_cumulative_mgd'),
+  'runlabel' = c('l90_Qout_dem', 'l30_Qout_dem', 'wd_cumulative_mgd', 'ps_cumulative_mgd')
 )
 metric_data <- om_vahydro_metric_grid(
   metric = metric, runids = df_metrics, bundle = 'all', ftype = "all",
@@ -54,18 +65,42 @@ metric_data <- om_vahydro_metric_grid(
   ds = ds
 )
 
-#convert cfs to mgd 
-metric_data$l30_Qout_mgd = metric_data$l30_Qout / 1.547
-metric_data$l90_Qout_mgd = metric_data$l90_Qout / 1.547
 
-#again, hard-coded runids (temporary)
+###--- Unit Conversions  ---###
+
+#Convert storage from mg to mgd 
+storage_data$SminL30mgd_11 <- storage_data$SminL30mg_11 / 30 #dividing by # of days in low flow period 
+storage_data$SminL30mgd_13 <- storage_data$SminL30mg_13 / 30
+storage_data$SminL90mgd_11 <- storage_data$SminL90mg_11 / 30
+storage_data$SminL90mgd_13 <- storage_data$SminL90mg_13 / 30
+
+#Convert cfs to mgd 
+#demand scenario
+metric_data$l30_Qout_dem_mgd = metric_data$l30_Qout_dem / 1.547
+metric_data$l90_Qout_dem_mgd = metric_data$l90_Qout_dem / 1.547
+
+#baseline scenario
+baseline_data$l30_Qout_base_mgd = baseline_data$l30_Qout_base / 1.547
+baseline_data$l90_Qout_base_mgd = baseline_data$l90_Qout_base / 1.547
+
+
+###--- Join Rseg Metric Data with Storage Data  ---###
+
+#Join storage data onto demand scenario metric data 
 metric_data <- sqldf('SELECT a.*, b.SminL30mgd_11, b.SminL90mgd_11
                    FROM metric_data as a
                    LEFT OUTER JOIN storage_data as b
-                   ON (a.riverseg = b.riverseg)') #joining rseg data to storage data
+                   ON (a.riverseg = b.riverseg)') 
+
+#Join baseline scenario data onto demand scenario & storage data 
+metric_data <- sqldf('SELECT a.*, b.l30_Qout_base_mgd, b.l90_Qout_base_mgd
+                   FROM metric_data as a
+                   LEFT OUTER JOIN baseline_data as b
+                   ON (a.riverseg = b.riverseg)') 
 
 
-#Linking riversegs with upstream impoundments 
+###--- Linking Riversegs with Upstream Impoundments  ---###
+
 metric_data$SminL30_total <- NA #empty rows to distinguish upstream storage
 metric_data$SminL90_total <- NA 
 
@@ -82,12 +117,17 @@ for (i in 1:nrow(metric_data)) {
   }
 }
 
+#Rename original Smin metrics from VAhydro as local storage
 colnames(metric_data)[which(names(metric_data) == "SminL90mgd_11")] <- "SminL30_local"
 colnames(metric_data)[which(names(metric_data) == "SminL90mgd_11")] <- "SminL90_local"
 
 metric_data[is.na(metric_data)] <- 0 #replace NA storage values with 0
 
-#solve for WA
+
+
+
+###--- Solving for Water Availability ---###
+
 metric_data$WA_L30_mgd = metric_data$l30_Qout_mgd - 0.9*(metric_data$l30_Qout_mgd + metric_data$wd_cumulative_mgd - metric_data$ps_cumulative_mgd) + metric_data$SminL30_total 
 metric_data$WA_L90_mgd = metric_data$l90_Qout_mgd - 0.9*(metric_data$l90_Qout_mgd + metric_data$wd_cumulative_mgd - metric_data$ps_cumulative_mgd) + metric_data$SminL90_total 
 
