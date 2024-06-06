d00baa61e9de1e87ebd117d2db554bdbfcec004d
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2024-06-06 17:47:36 -0400

    add in usgs data, and a comparison table using sqldf

--- HARP-2024-2025/access-db.R
+++ HARP-2024-2025/access-db.R
@@ -1,9 +1,6 @@
 library("RPostgres")
 library("sqldf")
-# RpostgreSQL is an older version?
-# library("RPostgreSQL")
-# require("RPostgreSQL")
-# this completes installing packages
+library("dataRetrieval")
 
 # works with only Rpostgres
 con <- dbConnect(
@@ -49,22 +46,6 @@ get_feature_met <- function(con, hydrocode, varkey, band = 1, mcovcode = 'cbp6_m
   return(dat)
 }
 
-# Average met
-
-all_met <- sqldf("select met.featureid, to_timestamp(met.tstime) as obs_date,
-        extract(month from to_timestamp(met.tstime)) as mo, 
-        (ST_summarystats(met.rast, 10, TRUE)).mean as precip_mm,
-        0.0393701 * (ST_summarystats(met.rast, 10, TRUE)).mean as precip_in
-from dh_feature as mcov
-left outer join dh_variabledefinition as v
-on (v.varkey = 'nldas2_obs_hourly')
-left outer join dh_timeseries_weather as met
-on ( mcov.hydroid = met.featureid and met.varid = v.hydroid)
-where mcov.hydrocode = 'cbp6_met_coverage'
-and extract(month from to_timestamp(met.tstime)) = 5
-order by met.tstime", connection = con)
-
-
 # Inventory
 inv_met <- sqldf("select extract(year from to_timestamp(met.tstime)) as year,
 min(to_timestamp(met.tstime)) as start_date,
@@ -94,3 +75,34 @@ order by extract(year from to_timestamp(met.tstime))", connection = con)
 
 
 sqldf("select max(tid) from dh_timeseries", connection = con)
+gageid = '01634000'
+hydrocode = paste0('usgs_ws_', gageid)
+prism_data <- get_feature_met(con, hydrocode, 'prism_mod_daily')
+prism_data[,c('yr', 'mo', 'da')] <- cbind(year(as.Date(prism_data$obs_date)), month(as.Date(prism_data$obs_date)), day(as.Date(prism_data$obs_date)) )
+
+daymet_data <- get_feature_met(con, hydrocode, 'daymet_mod_daily')
+daymet_data[,c('yr', 'mo', 'da')] <- cbind(year(as.Date(daymet_data$obs_date)), month(as.Date(daymet_data$obs_date)), day(as.Date(daymet_data$obs_date)) )
+# USGS
+gage_info <- memo_readNWISsite(gageid)
+da <- gage_info$drain_area_va
+usgs_data <- memo_readNWISdv(gageid,'00060')
+usgs_data[,c('yr', 'mo', 'da')] <- cbind(year(as.Date(usgs_data$Date)), month(as.Date(usgs_data$Date)), day(as.Date(usgs_data$Date)) )
+
+comp_data <- sqldf(
+  "select a.obs_date, a.precip_in as prism_p_in, 
+  b.precip_in as daymet_p_in, c.X_00060_00003 as usgs_cfs
+  from prism_data as a
+  left outer join daymet_data as b 
+  on (
+    a.yr = b.yr
+    and a.mo = b.mo
+    and a.da = b.da
+  )
+  left outer join usgs_data as c 
+  on (
+    a.yr = c.yr
+    and a.mo = c.mo
+    and a.da = c.da
+  )
+  "
+)
