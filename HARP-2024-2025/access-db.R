31679bf4e1972ea2d86b7b36285ac9875615428f
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2024-06-06 17:27:46 -0400

    functionalize

--- HARP-2024-2025/access-db.R
+++ HARP-2024-2025/access-db.R
@@ -5,16 +5,6 @@ library("sqldf")
 # require("RPostgreSQL")
 # this completes installing packages
 
-# now start creating connection
-# Works with RpostgreSQL
-con <- dbConnect(
-  dbDriver("PostgreSQL"),
-  dbname = "drupal.dh03",
-  host = "deq1.bse.vt.edu",
-  port = 5431,
-  user = readline("DB User Name: "),
-  password =  getPass::getPass("REST Password: ")
-)
 # works with only Rpostgres
 con <- dbConnect(
   RPostgres::Postgres(),
@@ -27,11 +17,43 @@ con <- dbConnect(
 
 # this completes creating connection
 
+
+get_feature_met <- function(con, hydrocode, varkey, band = 1, mcovcode = 'cbp6_met_coverage') {
+  q = paste0(
+    "select met.featureid, to_timestamp(met.tstime) as obs_date,
+      extract(month from to_timestamp(met.tstime)) as mo, 
+      (ST_summarystats(st_clip(met.rast, fgeo.dh_geofield_geom), ",
+    band, ", TRUE)).mean as precip_mm,
+      0.0393701 * (ST_summarystats(st_clip(met.rast, fgeo.dh_geofield_geom), ",
+    band, ", TRUE)).mean as precip_in
+    from dh_feature as f
+    left outer join field_data_dh_geofield as fgeo
+    on (
+      fgeo.entity_id = f.hydroid
+          and fgeo.entity_type = 'dh_feature'
+    )
+    left outer join dh_variabledefinition as v
+    on (v.varkey = '",
+    varkey, "')
+    left outer join dh_feature as mcov
+    on (
+      mcov.hydrocode = '", mcovcode, "'
+    )
+    left outer join dh_timeseries_weather as met
+    on ( mcov.hydroid = met.featureid and met.varid = v.hydroid)
+    where f.hydrocode = '", hydrocode, "'
+    order by met.tstime
+"
+  )
+  dat <- sqldf(q, connection = con)
+  return(dat)
+}
+
 # Average met
 
 all_met <- sqldf("select met.featureid, to_timestamp(met.tstime) as obs_date,
         extract(month from to_timestamp(met.tstime)) as mo, 
-        (ST_summarystats(met.rast, 10, TRUE)).mean as precip_kgm3,
+        (ST_summarystats(met.rast, 10, TRUE)).mean as precip_mm,
         0.0393701 * (ST_summarystats(met.rast, 10, TRUE)).mean as precip_in
 from dh_feature as mcov
 left outer join dh_variabledefinition as v
@@ -69,28 +91,6 @@ group by extract(year from to_timestamp(met.tstime))
 order by extract(year from to_timestamp(met.tstime))", connection = con)
 
 # get all the tables from connection
-
-gage_met <- sqldf("select met.featureid, to_timestamp(met.tstime) as obs_date,
-        extract(month from to_timestamp(met.tstime)) as mo, (ST_MemSize(st_clip(met.rast, fgeo.dh_geofield_geom))/1024)/1024 as size_mb,
-        (ST_summarystats(st_clip(met.rast, fgeo.dh_geofield_geom), 10, TRUE)).mean as precip_kgm3,
-        0.0393701 * (ST_summarystats(st_clip(met.rast, fgeo.dh_geofield_geom), 10, TRUE)).mean as precip_in
-from dh_feature as f
-left outer join field_data_dh_geofield as fgeo
-on (
-  fgeo.entity_id = f.hydroid
-      and fgeo.entity_type = 'dh_feature'
-)
-left outer join dh_variabledefinition as v
-on (v.varkey = 'nldas2_obs_hourly')
-left outer join dh_feature as mcov
-on (
-  mcov.hydrocode = 'cbp6_met_coverage'
-)
-left outer join dh_timeseries_weather as met
-on ( mcov.hydroid = met.featureid and met.varid = v.hydroid)
-where f.hydrocode = 'usgs_ws_01634000'
-order by met.tstime", connection = con)
-
 
 
 sqldf("select max(tid) from dh_timeseries", connection = con)
