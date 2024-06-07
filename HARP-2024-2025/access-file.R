c937333d789cc0d9d2287205930dc421bd42d01f
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2024-06-07 10:18:58 -0400

    added weekly summary and comparisons

--- HARP-2024-2025/access-file.R
+++ HARP-2024-2025/access-file.R
@@ -5,10 +5,10 @@ library("dataRetrieval")
 gageid = '01634000'
 hydrocode = paste0('usgs_ws_', gageid)
 prism_data <- read.csv("http://deq1.bse.vt.edu:81/files/met/usgs_ws_01634000-prism-2022-2023.csv")
-prism_data[,c('yr', 'mo', 'da')] <- cbind(year(as.Date(prism_data$obs_date)), month(as.Date(prism_data$obs_date)), day(as.Date(prism_data$obs_date)) )
+prism_data[,c('yr', 'mo', 'da', 'wk')] <- cbind(year(as.Date(prism_data$obs_date)), month(as.Date(prism_data$obs_date)), day(as.Date(prism_data$obs_date)), week(as.Date(prism_data$obs_date)) )
 
 daymet_data <- read.csv("http://deq1.bse.vt.edu:81/files/met/usgs_ws_01634000-daymet-2022-2023.csv")
-daymet_data[,c('yr', 'mo', 'da')] <- cbind(year(as.Date(daymet_data$obs_date)), month(as.Date(daymet_data$obs_date)), day(as.Date(daymet_data$obs_date)) )
+daymet_data[,c('yr', 'mo', 'da', 'wk')] <- cbind(year(as.Date(daymet_data$obs_date)), month(as.Date(daymet_data$obs_date)), day(as.Date(daymet_data$obs_date)), week(as.Date(daymet_data$obs_date)) )
 # USGS
 gage_info <- memo_readNWISsite(gageid)
 da <- gage_info$drain_area_va
@@ -18,7 +18,7 @@ usgs_data[,c('yr', 'mo', 'da')] <- cbind(year(as.Date(usgs_data$Date)), month(as
 
 comp_data <- sqldf(
   "select a.obs_date, a.precip_in as prism_p_in, 
-  a.yr, a.mo, a.da,
+  a.yr, a.mo, a.da, a.wk,
   b.precip_in as daymet_p_in, c.X_00060_00003 as usgs_cfs
   from prism_data as a
   left outer join daymet_data as b 
@@ -39,11 +39,16 @@ comp_data <- sqldf(
 comp_data$dataset_day <- index(comp_data) 
 comp_data <- sqldf(
   "select a.*, b.usgs_cfs as nextday_usgs_cfs, 
-  b.usgs_cfs - a.usgs_cfs as nextday_d_cfs
+  b.usgs_cfs - a.usgs_cfs as nextday_d_cfs, 
+  a.usgs_cfs - c.usgs_cfs as today_d_cfs
   from comp_data as a
   left outer join comp_data as b 
   on (
     a.dataset_day = (b.dataset_day - 1)
+  )
+  left outer join comp_data as c 
+  on (
+    a.dataset_day = (c.dataset_day + 1)
   )
   order by a.dataset_day"
 )
@@ -57,22 +62,40 @@ comp_data <- sqldf(
 # 1.572 * (DAsqmi * 640.0 * p_in / 12.0) / 3.07 
 comp_data$prism_p_cfs <- 1.572 * (da * 640.0 * comp_data$prism_p_in / 12.0) / 3.07 
 comp_data$daymet_p_cfs <- 1.572 * (da * 640.0 * comp_data$daymet_p_in / 12.0) / 3.07 
+week_data <- sqldf(
+  "select min(obs_date) as week_begin, yr, wk, min(dataset_day) as dataset_day_begin,
+     avg(daymet_p_in) as daymet_p_in, avg(daymet_p_cfs) as daymet_p_cfs,
+     avg(prism_p_in) as prism_p_in, avg(prism_p_cfs) as prism_p_cfs,
+     avg(usgs_cfs) as usgs_cfs, avg(today_d_cfs) as today_d_cfs, 
+     avg(nextday_d_cfs) as nextday_d_cfs
+   from comp_data
+   group by yr, wk
+   order by yr, wk
+  "
+)
 
 mod_prism <- lm(usgs_cfs ~ prism_p_cfs, data=comp_data)
 summary(mod_prism)
 
 mod_daymet <- lm(usgs_cfs ~ daymet_p_cfs, data=comp_data)
 summary(mod_daymet)
+# Weekly cfs vs P
+mod_week_prism <- lm(usgs_cfs ~ prism_p_cfs, data=week_data)
+summary(mod_week_prism)
+mod_week_daymet <- lm(usgs_cfs ~ daymet_p_cfs, data=week_data)
+summary(mod_week_daymet)
 
 
+# January only
+# PRISM
 mod_prism_jan <- lm(usgs_cfs ~ prism_p_cfs, data=comp_data[which(comp_data$mo == 1),])
 summary(mod_prism_jan)
-
+# daymet
 mod_daymet_jan <- lm(usgs_cfs ~ daymet_p_cfs, data=comp_data[which(comp_data$mo == 1),])
 summary(mod_daymet_jan)
 plot(mod_daymet_jan$model$usgs_cfs ~ mod_daymet_jan$model$daymet_p_cfs)
 
-
+# January, next day flow todays precip
 mod_prism_jan_nd <- lm(nextday_usgs_cfs ~ prism_p_cfs, data=comp_data[which(comp_data$mo == 1),])
 summary(mod_prism_jan_nd)
 
@@ -86,6 +109,18 @@ mod_daymet_jan_ndd <- lm(nextday_d_cfs ~ daymet_p_cfs, data=comp_data[which(comp
 summary(mod_daymet_jan_ndd)
 plot(mod_daymet_jan_ndd$model$nextday_d_cfs ~ mod_daymet_jan_ndd$model$daymet_p_cfs)
 mod_daymet_jan_ndd$model
+
+# only compare against change in flows on the same day as non-zero rain
+# *** DAYMET
+mod_daymet_jan_nz_ndd <- lm(nextday_d_cfs ~ daymet_p_cfs, data=comp_data[which((comp_data$mo == 1) & (comp_data$daymet_p_cfs > 0)),])
+summary(mod_daymet_jan_nz_ndd)
+plot(mod_daymet_jan_nz_ndd$model$nextday_d_cfs ~ mod_daymet_jan_nz_ndd$model$daymet_p_cfs)
+mod_daymet_jan_nz_ndd$model
+# *** PRISM
+mod_prism_jan_nz_ndd <- lm(nextday_d_cfs ~ prism_p_cfs, data=comp_data[which((comp_data$mo == 1) & (comp_data$prism_p_cfs > 0)),])
+summary(mod_prism_jan_nz_ndd)
+plot(mod_prism_jan_nz_ndd$model$nextday_d_cfs ~ mod_prism_jan_nz_ndd$model$prism_p_cfs)
+mod_prism_jan_nz_ndd$model
 
 # only compare against change in flows on the day after it rained
 # *** DAYMET
