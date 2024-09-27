ea0f9483f950d5eb5a9e7d89980e27674e5a7631
Mahadwar <Gouri.Mahadwar@deq.virginia.gov>
2024-09-27 16:26:34 -0400

    set floor and ceiling to filter unmet demand table to only include vlaues

--- HARP-2023-Summer/Mapping/Config/mapstyle_config.R
+++ HARP-2023-Summer/Mapping/Config/mapstyle_config.R
@@ -162,8 +162,8 @@ run_sets <- list(
         ramp = 'default',
         tables_cols = c('Facility', 'Facility_hydroid', 'riverseg', 'runid_11_unmet30_mgd',  'runid_13_unmet30_mgd', 'runid_17_unmet30_mgd' ,'gw_frac'),
         sort_col = 'runid_11_unmet30_mgd',
-        floor = NULL, 
-        ceiling = NULL, 
+        floor = 0.001, 
+        ceiling = 1000, 
         n_entries = 25,
         sort = 'decreasing',
         exlude_NAs = FALSE
