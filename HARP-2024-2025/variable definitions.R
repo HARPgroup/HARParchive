93c19c326435e3ae033b8bceb2ce84881b805260
Nate Foley <nathanielf22@vt.edu>
2024-11-08 09:49:41 -0500

    updates for integration

--- HARP-2024-2025/variable definitions.R
+++
@@ -1,6 +0,0 @@
-coverage_hydrocode <- "usgs_ws_01613900"
-coverage_bundle <- "watershed" #type of coverage we are looking for
-coverage_ftype <- "usgs_full_drainage" #feature type
-model_version <- "cbp-6.1"
-met_file <- "http://deq1.bse.vt.edu:81/met/stormVol_prism/precip/usgs_ws_01613900-PRISM-all.csv"
-scenario_name <- "stormVol_prism"
