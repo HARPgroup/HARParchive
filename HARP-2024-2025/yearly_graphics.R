fc31fe5ea0d24a33374520dcfd0d9111dc0355a3
Nate Foley <nathanielf22@vt.edu>
2024-11-19 11:27:34 -0500

    yearly graphic for Hogue Creek

--- HARP-2024-2025/yearly_graphics.R
+++ HARP-2024-2025/yearly_graphics.R
@@ -21,34 +21,46 @@ yearly_summary <- function(df){
   
 }
 
-df1 <- yearly_summary(prism)
-df2 <- yearly_summary(daymet)
-df3 <- yearly_summary(nldas2)
-p1 <- ggplot(df1, mapping = aes(x = yr, y = total_precip))+
+prism <- read.csv("http://deq1.bse.vt.edu:81/met/stormVol_prism/precip/usgs_ws_01613900-PRISM-all.csv")
+daymet <- read.csv("http://deq1.bse.vt.edu:81/met/daymet/precip/usgs_ws_01613900-daymet-all.csv")
+nldas2 <- read.csv("http://deq1.bse.vt.edu:81/met/nldas2/precip/usgs_ws_01613900-nldas2-all.csv")
+
+prism_sum <- yearly_summary(prism)
+daymet_sum <- yearly_summary(daymet)
+nldas_sum <- yearly_summary(nldas2)
+
+
+# Example plots for each 
+ggplot(prism_sum, mapping = aes(x = yr, y = total_precip))+
   geom_histogram(stat = "identity", binwidth = 1, fill = "lightblue1")+
   theme_bw()
 
-p2 <- ggplot(df2, mapping = aes(x = yr, y = total_precip))+
+ggplot(daymet_sum, mapping = aes(x = yr, y = total_precip))+
   geom_histogram(stat = "identity", binwidth = 1, fill = "lightblue2")+
   theme_bw()
 
-p3 <- ggplot(df3, mapping = aes(x = yr, y = total_precip))+
+ggplot(nldas_sum, mapping = aes(x = yr, y = total_precip))+
   geom_histogram(stat = "identity", binwidth = 1, fill = "lightblue3")+
   theme_bw()
-p1 / p2 / p3
 
+
+# A series of SQL joins and unions that produce a tidy, combined data frame
+
+## joins prism and daymet
 j <- sqldf(
-  "SELECT df2.yr , df1.total_precip AS prism_precip , df2.total_precip AS daymet_precip
-  FROM df1
-  FULL OUTER JOIN df2 ON df1.yr=df2.yr"
+  "SELECT daymet_sum.yr , prism_sum.total_precip AS prism_precip , daymet_sum.total_precip AS daymet_precip
+  FROM prism_sum
+  FULL OUTER JOIN daymet_sum ON prism_sum.yr=daymet_sum.yr"
 )
 
+## joins nldas to prism&daymet data frame
 j2 <- sqldf(
-  "SELECT j.yr , j.prism_precip , j.daymet_precip, df3.total_precip AS nldas_precip
+  "SELECT j.yr , j.prism_precip , j.daymet_precip, nldas_sum.total_precip AS nldas_precip
   FROM j
-  FULL OUTER JOIN df3 on j.yr=df3.yr"
+  FULL OUTER JOIN nldas_sum on j.yr=nldas_sum.yr"
 )
 
+## tidies data -- pivot longer
 j3 <- sqldf(
   "SELECT yr , 'prism' AS model , prism_precip AS precip_in FROM j2
   UNION ALL
@@ -57,13 +69,15 @@ j3 <- sqldf(
   SELECT yr , 'nldas' AS model , nldas_precip AS precip_in FROM j2"
 )
 
-ggplot(j3, aes(x = yr, y = precip_in, fill = model))+
-  geom_histogram(stat = "identity", position = "identity", binwidth = 1, alpha = 0.5)+
-  geom_histogram(stat = "identity", position = "identity", binwidth = 1, alpha = 0.5)+
-  theme_bw() #add border for visibility
 
-p1
-p2
-p3
-p1 / p2 /p3
+# Plot with all models and years
+
+yearly_precip_plot <- ggplot(j3, aes(x = yr, y = precip_in, fill = model, color = model))+
+  geom_histogram(stat = "identity", position = "identity", binwidth = 1, alpha = 0.4)+
+  theme_bw()+
+  labs(title = 
+  "Yearly Precip Model Comparison
+  Hogue Creek Near Hayfield VA", x = "Year", y = "Precipitation (in)")
+
+
 
