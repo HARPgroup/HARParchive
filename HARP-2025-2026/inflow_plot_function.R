179f9f0d3a470bf4f721ad646df80ed2a6b85b93
Ilona Hughes <ilonah22@vt.edu>
2025-07-09 14:53:45 -0400

    Updated with barplots

--- HARP-2025-2026/inflow_plot_function.R
+++ HARP-2025-2026/inflow_plot_function.R
@@ -83,10 +83,10 @@ H51165 <- add.season(H51165, "index")
 N51165 <- add.season(N51165, "index")
 N51171 <- add.season(N51171, "index")
 N54031 <- add.season(N54031, "index")
-H51165_daily <- add.season(N51165_daily, "Date")
+H51165_daily <- add.season(H51165_daily, "Date")
 N51165_daily <- add.season(N51165_daily, "Date")
-N51171_daily <- add.season(N51165_daily, "Date")
-N54031_daily <- add.season(N51165_daily, "Date")
+N51171_daily <- add.season(N51171_daily, "Date")
+N54031_daily <- add.season(N54031_daily, "Date")
 
 plot.zero.inflows(N51165, 
                   landseg = "N51165", 
@@ -106,9 +106,10 @@ plot.zero.inflows(N51171,
                   zone = "UZ", 
                   second_axis = "S")
 
-plot.inflows <- function(gw_daily_data, landseg, landtype, 
+plot.inflows <- function(gw_data, landseg, landtype, 
                               zone = c("AGW", "LZ", "UZ"), 
-                              second_axis = c("ET", "O", "S")) {
+                              second_axis = c("ET", "O", "S"),
+                         plot_type = c("bar", "box")) {
   require(ggplot2)
   require(patchwork)
   
@@ -116,7 +117,7 @@ plot.inflows <- function(gw_daily_data, landseg, landtype,
   inflow_col <- paste0(zone, "I")
   second_col <- paste0(zone, second_axis)
   
-  inflow <- gw_daily_data
+  inflow <- gw_data
 
   # Set correct units
   if(second_axis == "S"){
@@ -142,13 +143,38 @@ plot.inflows <- function(gw_daily_data, landseg, landtype,
   inflow[["Month"]] <- factor(inflow[["month"]], levels = 1:12, labels = month.abb)
   monthly_sc$Month <- factor(monthly_sc$Month, levels = 1:12, labels = month.abb)
   
+
+  if(plot_type == "box"){
+  inflow <- add.season(inflow, "index")  
+  
   p1 <- ggplot(inflow, aes(x = Month, fill = Season, color = Season)) +
-    geom_boxplot(aes(y = inflow[[inflow_col]]), size = 0.5, alpha = 0.65) +
+    geom_boxplot(coef = 100,
+                 aes(y = inflow[[inflow_col]]),
+                 size = 0.5, alpha = 0.65) +
     theme_bw()+
-    coord_cartesian(ylim = c(0, quantile(inflow[[inflow_col]], 0.80)))+
+    coord_cartesian(ylim = c(0, quantile(inflow[[inflow_col]], 0.99)))+
     scale_fill_manual(values = c("orangered3", "midnightblue", "darkolivegreen3", "maroon3"))+
     scale_color_manual(values = c("orangered3", "midnightblue", "darkolivegreen3", "maroon3"))+
     ylab(paste0(inflow_col, " (in/day)"))
+  
+  }else{
+    
+    # Create sums and averages
+    monthly_totals <- aggregate(inflow[[inflow_col]] ~ year + month, data = inflow, FUN = sum)
+    names(monthly_totals)[3] <- "monthly_volume"
+    df_bar <- aggregate(monthly_volume ~ month, data = monthly_totals, FUN = mean)
+    names(df_bar)[2] <- "mean_in"
+    df_bar <- add.season(df_bar, "month")
+    
+  
+    p1 <- ggplot(df_bar, aes(x = month, y = mean_in, fill = Season, color = Season)) +
+      geom_bar(stat = "identity", size = 0.5, alpha = 0.65) +
+      theme_bw() +
+      coord_cartesian(ylim = c(0, quantile(df_bar$mean_inflow, 0.8))) +
+      scale_fill_manual(values = c("orangered3", "midnightblue", "darkolivegreen3", "maroon3")) +
+      scale_color_manual(values = c("orangered3", "midnightblue", "darkolivegreen3", "maroon3")) +
+      ylab(paste0(inflow_col, " (in/month)"))
+  }
   
   p2 <- ggplot(monthly_sc, aes(x = Month, y = x)) +
     geom_line(color = "firebrick4", size = 1.05, group = 1) +
@@ -158,23 +184,30 @@ plot.inflows <- function(gw_daily_data, landseg, landtype,
   # add plots together
   plot <- p1 / p2
   plot <- plot + 
-    plot_annotation(title = paste0("Seasonal Trends in Groundwater of ", landtype, " Land in ", landseg )) & 
+    plot_annotation(title = paste0("Seasonal Trends in Water of ", landtype, " Land in ", landseg )) & 
     theme(plot.title = element_text(hjust = 0.5))
   
   return(plot)
 }
 
+
+
+plot.inflows(H51165, 
+             landseg = "H51165", 
+             landtype = "Forested", 
+             zone = "AGW", 
+             second_axis = "O",
+             plot_type = "bar")
+
+
 plot.inflows(N51165_daily, 
               landseg = "N51165", 
               landtype = "Forested", 
               zone = "AGW", 
-              second_axis = "O")
+              second_axis = "O",
+             plot_type = "bar")
 
-plot.inflows(H51165_daily, 
-              landseg = "H51165", 
-              landtype = "Forested", 
-              zone = "LZ", 
-              second_axis = "ET")
+
 
 plot.inflows(N51171_daily, 
               landseg = "N51171", 
