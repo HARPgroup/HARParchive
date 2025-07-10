6a10e99e3a761557f672d13c0f6ea12bf983c356
Ilona Hughes <ilonah22@vt.edu>
2025-07-10 15:39:19 -0400

    Updated recession event method

--- HARP-2025-2026/recession_events.R
+++ HARP-2025-2026/recession_events.R
@@ -22,16 +22,31 @@ usgs_flows$AGWRC <- usgs_flows$flow_cfs / c(NA, head(usgs_flows$flow_cfs, -1))
 # Create column for dAGWRC
 usgs_flows$dAGWRC <- usgs_flows$AGWRC / c(NA, head(usgs_flows$AGWRC, -1))
 
+usgs_flows$delta_dAGWRC <- usgs_flows$dAGWRC - c(NA, head(usgs_flows$dAGWRC, -1))
+
+f_diff <- 0.05 * mean(usgs_flows$dAGWRC, na.rm=T)
+
 # Determine Recession Periods
-usgs_flows <- sqldf(
-  "select *, FALSE as recession 
-  from usgs_flows where dAGWRC < 0.95
-  union all
-  select *, TRUE as recession
-  from usgs_flows where dAGWRC >= 0.95 and dAGWRC <= 1.05
-  order by dateTime
-  "
-)
+usgs_flows <- sqldf(sprintf(
+  "select *,
+  case when 
+    abs(delta_dAGWRC) <= %f then 'TRUE' 
+    else 'FALSE' 
+    end as recession
+   from usgs_flows", f_diff))
+
+
+
+# Limits determine the percent difference to count as recession 
+# usgs_flows <- sqldf(
+#   "select *, FALSE as recession 
+#   from usgs_flows where dAGWRC < 0.95
+#   union all
+#   select *, TRUE as recession
+#   from usgs_flows where dAGWRC >= 0.9 and dAGWRC <= 1.01
+#   order by dateTime
+#   "
+# )
 
 usgs_flows$recession <- as.logical(usgs_flows$recession)
 
@@ -67,24 +82,35 @@ event_sum$mean_AGWRC <- mean_AGWRC$`event_data$AGWRC`
 
 usgs_flows$dateTime <- as.Date(usgs_flows$dateTime)
 
-event_sum <- subset(event_sum, ndays > 1)
+event_sum <- subset(event_sum, ndays > 3)
+ 
+event_sum$gap_days <- c(NA, as.numeric(difftime(event_sum$start_date[-1], 
+                                                        event_sum$end_date[-nrow(event_sum)], 
+                                                        units = "days")))
+
 
 # Plotting A specific storm event ----
-i <- 94
+i <- 4078
 
-s_date <- event_sum$start_date[i] - 30
-e_date <- event_sum$end_date[i] + 30
-target_event <- event_sum$event_id[i]
+s_date <- event_sum$start_date[which(event_sum$event_id == i)] - 30
+e_date <- event_sum$end_date[which(event_sum$event_id == i)] + 30
+target_event <- event_sum$event_id[which(event_sum$event_id == i)]
 
-ggplot(data = usgs_flows, mapping = aes(x = dateTime)) +
-  geom_line(aes(y = flow_cfs)) +
-  geom_point(
-    data = subset(usgs_flows, event_id == target_event),
-    aes(y = flow_cfs),
-    color = "firebrick", size = 2) +
+# Define a scaling factor to bring AGWRC and dAGWRC to the same scale as flow_cfs
+scale_factor <- 100* (max(usgs_flows$flow_cfs, na.rm = TRUE) / 
+  max(c(usgs_flows$AGWRC, usgs_flows$dAGWRC), na.rm = TRUE))
+
+ggplot(data = usgs_flows, mapping = aes(x = dateTime))+
+  geom_line(aes(y = flow_cfs))+
+  geom_line(aes(y = AGWRC * scale_factor, colour = "AGWRC"))+
+  geom_line(aes(y = dAGWRC * scale_factor, colour = "dAGWRC"))+
+  scale_color_manual(values = c("AGWRC" = "darkorange1", "dAGWRC" = "dodgerblue3"))+
+  geom_point(data = subset(usgs_flows, event_id == target_event),
+    aes(y = flow_cfs), color = "firebrick", size = 1)+
   coord_cartesian(xlim = c(s_date, e_date), ylim = c(0, max(event_sum$mean_flow)))+
-  theme_bw()+
+  scale_y_continuous(name = "Flow (cfs)",
+    sec.axis = sec_axis(~ . / scale_factor, name = "AGWRC and dAGWRC (Ratio)"))+
   xlab("Date")+
-  ylab("Flow (cfs)")+
   ggtitle(paste0("Recession Event ", i))+
+  theme_bw()+
   theme(plot.title = element_text(hjust = 0.5))
